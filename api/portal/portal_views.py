9ad8b5b8087a099333e8694ab42727ce5b42020b
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-02-20 19:00:59 +0530

    devlop:getkarma-api

--- api/portal/portal_views.py
+++ api/portal/portal_views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from .serializers import PortalSerializer
 from utils.utils_views import CustomResponse
+from user.models import Student,TotalKarma
+import decouple
 
 
 # class AddPortal(APIView):
@@ -12,3 +14,15 @@ from utils.utils_views import CustomResponse
 #             return CustomResponse(response={"access_id": obj.access_id}).get_success_response()
 #         else:
 #             return CustomResponse(has_error=True, status_code=400, message=serializer.errors).get_failure_response()
+
+
+class GetKarma(APIView):
+    def get(self, request):
+        screatToken = request.headers.get("secretToken")
+        if screatToken == decouple.config("SECRET_KEY"):
+            mu_id = request.data.get("mu_id")
+            student = Student.objects.get(mu_id=mu_id)
+            karma = TotalKarma.objects.get(user_id=student.user_id)
+            return CustomResponse(response={"muid": mu_id, "fullname": student.fullname, "email": student.email, "karma": karma.karma}).get_success_response()
+        else:
+            return CustomResponse(has_error=True, status_code=400, message="token is incorrect").get_failure_response()
