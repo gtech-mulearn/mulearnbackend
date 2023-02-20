2c9959d9869abfa7de75de48908fc4c01ad62f40
nithindaniel1 <nithindaniel2018@gmail.com>
2023-02-20 19:01:23 +0530

    mu-id validation

--- api/portal/portal_views.py
+++ api/portal/portal_views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from .serializers import PortalSerializer
 from utils.utils_views import CustomResponse
+from portal.models import*
+from user.models import Student
+from decouple import config
 
 
 # class AddPortal(APIView):
@@ -12,3 +15,15 @@ from utils.utils_views import CustomResponse
 #             return CustomResponse(response={"access_id": obj.access_id}).get_success_response()
 #         else:
 #             return CustomResponse(has_error=True, status_code=400, message=serializer.errors).get_failure_response()
+
+class MuidValidate(APIView):
+    def post(self, request):
+        secretToken = request.headers.get('secretToken')
+        print(secretToken)
+        if secretToken == config('SECRET_KEY'):
+            name = request.data.get('name')
+            mu_id = request.data.get('mu_id')
+            user = Student.objects.get(mu_id=mu_id)        
+            return CustomResponse({"hasError":False,"statusCode":200,"message":"User Validated","response":{"name":user.fullname,"muId":user.mu_id}}).get_success_response()
+        else:
+            return CustomResponse({"hasError":True,"statusCode":404,"message":"Validation Falied","response":{}}).get_failure_response()
--- api/portal/urls.py
+++ api/portal/urls.py
@@ -3,6 +3,5 @@ from . import portal_views
 
 urlpatterns = [
     # path('add', portal_views.AddPortal.as_view()),
-
-
+    path('mu-id/validate',portal_views.MuidValidate.as_view()),
 ]
