67f1e324a7db887d5f2b13fa45cb6396c5b11b0d
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-03-13 02:41:33 +0530

    onboarding-backend-completed

--- api/user/user_views.py
+++ api/user/user_views.py
@@ -1,8 +1,96 @@
 from rest_framework.views import APIView
+from api.user.serializers import AreaOfInterstAPISerializer, OrgSerializer, RegisterSerializer, UserDetailSerializer
+from organization.models import Country, Department, Organization, State, Zone, District
+from rest_framework import viewsets
+from task.models import InterestGroup
+from user.models import Role, User
 
-from utils.utils_views import CustomResponse
+from utils.utils_views import CustomResponse, CustomizePermission
 
 
-class TestApi(APIView):
+class RegisterJWTValidte(APIView):
+    authentication_classes = [CustomizePermission]
+
     def get(self, request):
-        return CustomResponse(response={'hey': 'hey'}).get_success_response()
+        discord_id = request.auth.get('id', None)
+        if User.objects.filter(discord_id=discord_id).exists():
+            return CustomResponse(has_error=True, message='user already registerd', status_code=400).get_failure_response()
+        return CustomResponse(response={'token': True}).get_success_response()
+
+
+class RegisterData(APIView):
+    authentication_classes = [CustomizePermission]
+
+    def post(self, request):
+        data = request.data
+        create_user = RegisterSerializer(
+            data=data, context={'request': request})
+        if create_user.is_valid():
+            user_obj = create_user.save()
+            return CustomResponse(response={"data": UserDetailSerializer(user_obj, many=False).data}).get_success_response()
+        else:
+            return CustomResponse(has_error=True, status_code=400, message=create_user.errors).get_failure_response()
+        return CustomResponse(response={'token': data}).get_success_response()
+
+
+# class CountryAPI(APIView):
+#     authentication_classes = [CustomizePermission]
+
+#     def get(self, request):
+#         queryset = Country.objects.all()
+#         read_serializer_data = CountrySerializer(queryset, many=True).data
+#         countries = []
+#         for data in read_serializer_data:
+#             countries = [data['name']]
+#         return CustomResponse(response=countries).get_success_response()
+
+class RoleAPI(APIView):
+    authentication_classes = [CustomizePermission]
+
+    def get(self, request):
+        role_serializer = Role.objects.all()
+        role_serializer_data = OrgSerializer(
+            role_serializer, many=True).data
+        return CustomResponse(response={"roles": role_serializer_data}).get_success_response()
+
+
+class CollegeAPI(APIView):
+    authentication_classes = [CustomizePermission]
+
+    def get(self, request):
+        org_queryset = Organization.objects.filter(org_type="college")
+        department_queryset = Department.objects.all()
+        college_serializer_data = OrgSerializer(org_queryset, many=True).data
+        department_serializer_data = OrgSerializer(
+            department_queryset, many=True).data
+        return CustomResponse(response={"colleges": college_serializer_data, "departments": department_serializer_data}).get_success_response()
+
+
+class CompanyAPI(APIView):
+    authentication_classes = [CustomizePermission]
+
+    def get(self, request):
+        company_queryset = Organization.objects.filter(org_type="company")
+        company_serializer_data = OrgSerializer(
+            company_queryset, many=True).data
+        return CustomResponse(response={"companies": company_serializer_data}).get_success_response()
+
+
+class ComunityAPI(APIView):
+    authentication_classes = [CustomizePermission]
+
+    def get(self, request):
+        community_queryset = Organization.objects.filter(org_type="comunity")
+        comunity_serializer_data = OrgSerializer(
+            community_queryset, many=True).data
+        return CustomResponse(response={"communities": comunity_serializer_data}).get_success_response()
+
+
+class AreaOfInterstAPI(APIView):
+    authentication_classes = [CustomizePermission]
+
+    def get(self, request):
+        aoi_queryset = InterestGroup.objects.all()
+        comunity_serializer_data = AreaOfInterstAPISerializer(
+            aoi_queryset, many=True).data
+        return CustomResponse(response={"aois": comunity_serializer_data}).get_success_response()
---
+++ utils/exception.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ValidationError
+
+
+class CustomException(ValidationError):
+    def __init__(self, detail='Something went wrong', status_code=403):
+        self.detail = detail
+        self.status_code = status_code
