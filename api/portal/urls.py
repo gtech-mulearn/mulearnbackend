adc12d20a82b28634d3acfac9efb7c7117bb6ccd
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-04-06 23:43:39 +0530

    changed to django url stanadards

--- api/portal/urls.py
+++ api/portal/urls.py
@@ -3,9 +3,9 @@ from . import portal_views
 
 urlpatterns = [
     # path('add', portal_views.AddPortal.as_view()),
-    path('mu-id/validate', portal_views.MuidValidate.as_view()),
-    path('user/authorize', portal_views.UserMailTokenValidation.as_view()),
-    path('profile/karma', portal_views.GetKarma.as_view()),
-    path('profile/user/<str:muid>', portal_views.UserDetailsApi.as_view()),
-    path('get-unverified-users', portal_views.GetUnverifiedUsers.as_view()),
+    path('mu-id/validate/', portal_views.MuidValidate.as_view()),
+    path('user/authorize/', portal_views.UserMailTokenValidation.as_view()),
+    path('profile/karma/', portal_views.GetKarma.as_view()),
+    path('profile/user/<str:muid>/', portal_views.UserDetailsApi.as_view()),
+    path('get-unverified-users/', portal_views.GetUnverifiedUsers.as_view()),
 ]
--- api/user/urls.py
+++ api/user/urls.py
@@ -3,15 +3,15 @@ from .user_views import RegisterJWTValidate, RegisterData, CollegeAPI, CompanyAP
     AreaOfInterestAPI, ForgotPasswordAPI, ResetPasswordConfirmAPI,ResetPasswordVerifyTokenAPI
 
 urlpatterns = [
-    path('register/jwt/validate', RegisterJWTValidate.as_view()),
+    path('register/jwt/validate/', RegisterJWTValidate.as_view()),
     path('register/', RegisterData.as_view()),
     # path('register/country/list', CountryAPI.as_view()),
-    path('register/role/list', RoleAPI.as_view()),
-    path('register/college/list', CollegeAPI.as_view()),
-    path('register/company/list', CompanyAPI.as_view()),
-    path('register/comunity/list', CommunityAPI.as_view()),
-    path('register/areaofinterst/list', AreaOfInterestAPI.as_view()),
-    path('forgot-password', ForgotPasswordAPI.as_view()),
+    path('register/role/list/', RoleAPI.as_view()),
+    path('register/college/list/', CollegeAPI.as_view()),
+    path('register/company/list/', CompanyAPI.as_view()),
+    path('register/community/list/', CommunityAPI.as_view()),
+    path('register/area-of-interest/list/', AreaOfInterestAPI.as_view()),
+    path('forgot-password/', ForgotPasswordAPI.as_view()),
     path('reset-password/<str:token>/', ResetPasswordConfirmAPI.as_view()),
     path('reset-password/verify-token/<str:token>/', ResetPasswordVerifyTokenAPI.as_view())
 ]
