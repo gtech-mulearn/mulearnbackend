033fb851f82e4773aac8478c9506be851fbd494c
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-04-15 11:45:40 +0530

    College Monthly LEaderboard Api

--- api/leaderboard/urls.py
+++ api/leaderboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('students', leadrboard_view.StudentsLeaderboard.as_view()),
     path('students-monthly', leadrboard_view.StudentsMonthlyLeaderboard.as_view()),
     path('college', leadrboard_view.CollegeLeaderboard.as_view()),
+    path('college-monthly', leadrboard_view.CollegeMonthlyLeaderboard.as_view())
 ]
