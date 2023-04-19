fea6044856a99e40d945018f5939e8acf8a19ea2
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-04-20 01:58:25 +0530

    completed organisation post api

--- api/organisation/organisation_views.py
+++ api/organisation/organisation_views.py
@@ -18,3 +18,13 @@ class GetInstitutions(APIView):
         organisations = Organization.objects.filter(org_type=organisation_type,district=district)
         organisation_serializer = OrganisationSerializer(organisations, many=True)
         return CustomResponse(response={'institutions': organisation_serializer.data}).get_success_response()
+
+
+class PostInstitution(APIView):
+
+    def post(self, request):
+        organisation_serializer = OrganisationSerializer(data=request.data)
+        if organisation_serializer.is_valid():
+            organisation_serializer.save()
+            return CustomResponse(response={'institution': organisation_serializer.data}).get_success_response()
+        return CustomResponse(response={'institution': organisation_serializer.errors}).get_failure_response()
--- api/organisation/urls.py
+++ api/organisation/urls.py
@@ -4,4 +4,5 @@ from . import organisation_views
 urlpatterns = [
     # path('add', portal_views.AddPortal.as_view()),
     path('institutes/<str:organisation_type>/', organisation_views.GetInstitutions.as_view()),
+    path('institutes/add', organisation_views.PostInstitution.as_view()),
 ]
