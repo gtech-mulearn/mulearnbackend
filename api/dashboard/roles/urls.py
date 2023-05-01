c323e6e9b69b40b56e20781b4b0ab736e1efcb35
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-05-01 18:23:39 +0400

    [MINOR] Patches

--- api/dashboard/roles/urls.py
+++ api/dashboard/roles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import roles_views
+from . import dash_roles_views
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('', roles_views.RolesAPI.as_view(),name="roles-api"),
+    path('', dash_roles_views.RolesAPI.as_view(),name="roles-api"),
 ]
--- api/dashboard/user/urls.py
+++ api/dashboard/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .dash_user_views import UserAPI
 
 urlpatterns = [
-    path('default/', UserAPI.as_view(), name='user-api'),
+    path('', UserAPI.as_view(), name='user-api'),
 ]
 
