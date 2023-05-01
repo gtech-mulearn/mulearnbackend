d052442c70898b6bbd333f4516fba4a0881ecb0e
Arjun M S <arjunachu123.aa@gmail.com>
2023-05-01 18:56:32 +0530

    Added Roles API

--- api/dashboard/roles/urls.py
+++ api/dashboard/roles/urls.py
@@ -3,5 +3,5 @@ from . import roles_views
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('', roles_views.HelloWorld.as_view()),
+    path('', roles_views.RolesAPI.as_view(),name="roles-api"),
 ]
