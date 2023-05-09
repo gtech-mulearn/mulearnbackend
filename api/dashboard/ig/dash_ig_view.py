60e74581196df94cff81d4ea7649fef93fbe7b62
Jenin82 <jenin8282@gmail.com>
2023-05-10 00:53:09 +0530

    added permission for ig api

--- api/dashboard/ig/dash_ig_view.py
+++ api/dashboard/ig/dash_ig_view.py
@@ -1,14 +1,17 @@
 from rest_framework.views import APIView
 from db.task import InterestGroup
+from utils.permission import CustomizePermission, RoleRequired
 from utils.response import CustomResponse
+from utils.types import RoleType
 from .dash_ig_serializer import InterestGroupSerializer
 
 
 class InterestGroupAPI(APIView):
+    authentication_classes = [CustomizePermission] #for logged in users
+    @RoleRequired(roles=[RoleType.ADMIN, ]) #for admin
     def get(self, request):
         ig_serializer = InterestGroup.objects.all()
         ig_serializer_data = InterestGroupSerializer(ig_serializer, many=True).data
-
         return CustomResponse(
-            response={"InterestGroup": ig_serializer_data}
+            response={"interestGroups": ig_serializer_data}
         ).get_success_response()
