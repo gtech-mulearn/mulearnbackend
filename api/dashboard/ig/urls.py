028a47b30883d7933f4d9e6ff785cb6bd964515b
Jenin82 <jenin8282@gmail.com>
2023-05-11 10:05:27 +0530

    minor fix in ig url

--- api/dashboard/ig/urls.py
+++ api/dashboard/ig/urls.py
@@ -4,5 +4,5 @@ from . import dash_ig_view
 
 urlpatterns = [
     path('', dash_ig_view.InterestGroupAPI.as_view()), # for get data and create new interest groups
-    path('/<str:pk>/', dash_ig_view.InterestGroupAPI.as_view()), # for edit and delete
+    path('<str:pk>/', dash_ig_view.InterestGroupAPI.as_view()), # for edit and delete
 ]
