1902cea16319296441da79735bfa810585d3de45
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-05-11 19:25:18 +0400

    [MINOR] Code changes

--- api/dashboard/user/dash_user_views.py
+++ api/dashboard/user/dash_user_views.py
@@ -2,16 +2,36 @@ from rest_framework.views import APIView
 
 from db.user import User
 from utils.response import CustomResponse
+from utils.utils import CommonUtils
 
 from .dash_user_serializer import UserDashboardSerializer
 
 
 class UserAPI(APIView):
+    FIELDS = [
+        "id",
+        "discord_id",
+        "mu_id",
+        "first_name",
+        "last_name",
+        "email",
+        "mobile",
+        "gender",
+        "dob",
+        "admin",
+        "active",
+        "exist_in_guild",
+        "created_at",
+        "total_karma",
+    ]
+
     def get(self, request):
         
-        user_serializer = User.objects.all()
-        user_serializer_data = UserDashboardSerializer(user_serializer, many=True).data
-        
+        queryset = User.objects.all()
+        # queryset = CommonUtils.get_paginated_queryset(queryset, request, self.FIELDS)
+        serializer = UserDashboardSerializer(queryset, many=True)
+        serialized_data = serializer.data
+
         return CustomResponse(
-            response={"user": user_serializer_data}
+            response={"users": serialized_data}
         ).get_success_response()
