ac1447c60bd8a9580e7895b9208660ccc35d1540
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-05-15 22:04:49 +0400

    [FEAT] Add filter, paginate (User)

--- api/dashboard/user/dash_user_views.py
+++ api/dashboard/user/dash_user_views.py
@@ -1,9 +1,9 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from rest_framework.views import APIView
 
 from db.user import User
 from utils.response import CustomResponse
-from utils.utils import CommonUtils
-
 from .dash_user_serializer import UserDashboardSerializer
 
 
@@ -26,12 +26,40 @@ class UserAPI(APIView):
     ]
 
     def get(self, request):
-        
-        queryset = User.objects.all()
-        # queryset = CommonUtils.get_paginated_queryset(queryset, request, self.FIELDS)
-        serializer = UserDashboardSerializer(queryset, many=True)
+        query_params = request.GET
+        queryset = self.filter_users(query_params)
+        paginated_queryset = self.paginate_users(queryset, request)
+        serializer = UserDashboardSerializer(paginated_queryset, many=True)
         serialized_data = serializer.data
 
         return CustomResponse(
             response={"users": serialized_data}
         ).get_success_response()
+
+    def filter_users(self, query_params):
+        query_filters = Q()
+        filtered_params = {
+            key: value
+            for key, value in query_params.items()
+            if key in self.FIELDS and value
+        }
+
+        for key, value in filtered_params.items():
+            query_filters &= Q(**{f"{key}__icontains": value})
+
+        return User.objects.filter(query_filters)
+
+    def paginate_users(self, queryset, request):
+        per_page = 3
+        paginator = Paginator(queryset, per_page)
+
+        page = request.GET.get("page", 1)
+
+        try:
+            paginated_users = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_users = paginator.page(1)
+        except EmptyPage:
+            paginated_users = paginator.page(paginator.num_pages)
+
+        return paginated_users
