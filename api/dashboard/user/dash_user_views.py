60ab3d3e21d376857f828d349a8026f4416c6097
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-05-17 20:37:03 +0400

    [FEAT] list users

--- api/dashboard/user/dash_user_views.py
+++ api/dashboard/user/dash_user_views.py
@@ -1,26 +1,32 @@
+import uuid
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 
 from db.user import User
+from utils.permission import CustomizePermission, RoleRequired
 from utils.response import CustomResponse
+from utils.types import RoleType
+from utils.utils import CommonUtils, DateTimeUtils
+
 from .dash_user_serializer import UserDashboardSerializer
-from utils.utils import CommonUtils
 
 
 class UserAPI(APIView):
+    authentication_classes = [CustomizePermission]
 
     def get(self, request):
         user_queryset = User.objects.all()
-        
+
         queryset = CommonUtils.get_paginated_queryset(
             user_queryset, request, ["id", "first_name"]
         )
-        
+
         serializer = UserDashboardSerializer(queryset, many=True)
-        serialized_data = serializer.data
 
         return CustomResponse(
-            response={"users": serialized_data}
+            response={"users": serializer.data}
         ).get_success_response()
-        
-        
-    
+
+
+
