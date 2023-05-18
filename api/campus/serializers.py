2829e7a1423f8367551e902642efcf66385b434b
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-05-18 13:16:08 +0530

    develop:campus-student-details

--- api/campus/serializers.py
+++ api/campus/serializers.py
@@ -7,9 +7,53 @@ class UserOrgSerializer(serializers.ModelSerializer):
     name = serializers.ReadOnlyField(source="user.fullname")
     email = serializers.ReadOnlyField(source="user.email")
     phone = serializers.ReadOnlyField(source="user.mobile")
-    # totalKarma = serializers.SerializerMethodField()
+    karma = serializers.SerializerMethodField()
+    rank = serializers.SerializerMethodField()
+
     class Meta:
         model = TotalKarma
-        fields = ["name","email","phone","karma"]
+        fields = ["name", "email", "phone", "karma", "rank"]
 
-        
+    def get_karma(self, obj):
+        try:
+            karma = obj.user.total_karma_user.karma
+        except:
+            karma = 0
+        return karma
+
+    def get_rank(self, obj):
+        queryset = UserOrganizationLink.objects.order_by(
+            "-user__total_karma_user__karma")
+        rank = list(queryset).index(obj)
+        return rank
+
+
+class CollegeSerializer(serializers.ModelSerializer):
+    collegeName = serializers.ReadOnlyField(source="org.title")
+    campusCode = serializers.ReadOnlyField(source="org.code")
+    campusZone = serializers.ReadOnlyField(source="org.district.zone.name")
+    campusLead = serializers.ReadOnlyField(source="user.fullname")
+    totalKarma = serializers.SerializerMethodField()
+    totalMembers = serializers.SerializerMethodField()
+    activeMembers = serializers.SerializerMethodField()
+    rank = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserOrganizationLink
+        fields = ["collegeName", "campusLead", "campusCode",
+                  "campusZone", "totalKarma", "totalMembers", "activeMembers", "rank"]
+
+    def get_totalKarma(self, obj):
+        karma = 0
+        for user_org_link in obj.org.user_organization_link_org_id.filter(verified=True):
+            karma += user_org_link.user.total_karma_user.karma
+        return karma
+
+    def get_totalMembers(self, obj):
+        return obj.org.user_organization_link_org_id.count()
+
+    def get_activeMembers(self, obj):
+        return obj.org.user_organization_link_org_id.filter(verified=True, user__active=True).count()
+
+    def get_rank(self, obj):
+        return 1
--- api/campus/urls.py
+++ api/campus/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.campus.campus_views import StudentDetails
+from api.campus.campus_views import CampusDetails, StudentDetails
 
 urlpatterns = [
     path("student-details/", StudentDetails.as_view()),
+    path("campus-details/", CampusDetails.as_view()),
 ]
