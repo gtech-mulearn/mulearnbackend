b0b5a17224036812c6706991cd2d6808d01e2b53
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-05-18 18:24:24 +0400

    [MINOR] change endpoint

--- api/dashboard/roles/urls.py
+++ api/dashboard/roles/urls.py
@@ -4,7 +4,7 @@ from . import dash_roles_views
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
     path('', dash_roles_views.RoleAPI.as_view(),name="roles-list"),
+    path('', dash_roles_views.RoleAPI.as_view(),name="roles-create"),
     path('<str:roles_id>/', dash_roles_views.RoleAPI.as_view(),name="roles-edit"),
     path('<str:roles_id>/', dash_roles_views.RoleAPI.as_view(),name="roles-delete"),
-    path('create-role/', dash_roles_views.RoleAPI.as_view(),name="roles-create"),
 ]
