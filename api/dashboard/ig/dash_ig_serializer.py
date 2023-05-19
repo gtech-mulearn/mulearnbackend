2ac274a9451370d43bbe655655b1f12be0f3e5b5
Jenin82 <jenin8282@gmail.com>
2023-05-19 18:48:25 +0530

    interest group created user name and member count

--- api/dashboard/ig/dash_ig_serializer.py
+++ api/dashboard/ig/dash_ig_serializer.py
@@ -2,13 +2,20 @@ from rest_framework import serializers
 from db.task import InterestGroup
 
 class InterestGroupSerializer(serializers.ModelSerializer):
+    updated_by = serializers.CharField(source='updated_by.fullname')
+    created_by = serializers.CharField(source='created_by.fullname')
+    count = serializers.SerializerMethodField()
     class Meta:
         model = InterestGroup
         fields = [
             "id",
             "name",
+            "count",
             "updated_by",
             "updated_at",
             "created_by",
             "created_at",
         ]
+    
+    def get_count(self, obj):
+        return len(obj.user_ig_link_ig.all())
