6bb91d7b8909a6b08486a96b31207a9430078b72
Aashish <8xdeadshot@gmail.com>
2023-05-22 18:20:07 +0530

    Task list [EDIT , DELETE, VIEW ,CREATE]

---
+++ api/dashboard/task/dash_task_serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from db.task import TaskList
+
+
+class TaskListSerializer(serializers.ModelSerializer):
+    created_by = serializers.CharField(source='created_by.fullname')
+    updated_by = serializers.CharField(source='created_by.fullname')
+
+    class Meta:
+        model = TaskList
+        fields = [
+            "id",
+            "hashtag",
+            "title",
+            "description",
+            "karma",
+            "channel",
+            "type",
+            "active",
+            "variable_karma",
+            "usage_count",
+            "updated_at",
+            "updated_by",
+            "created_by",
+            "created_at"
+        ]
