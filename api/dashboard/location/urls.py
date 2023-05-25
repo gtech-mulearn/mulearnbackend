cb87d3ffd2b965c15063b8dc800b4f61bd7527a6
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-05-25 14:10:32 +0530

    added API to end of all classes in location api

--- api/dashboard/location/urls.py
+++ api/dashboard/location/urls.py
@@ -3,8 +3,8 @@ from . import location_views
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('country', location_views.CountryData.as_view()),
-    path('<str:country>/states', location_views.StateData.as_view()),
-    path('<str:country>/<str:state>/zone', location_views.ZoneData.as_view()),
-    path('<str:country>/<str:state>/<str:zone>/district', location_views.DistrictData.as_view()),
+    path('country', location_views.CountryDataAPI.as_view()),
+    path('<str:country>/states', location_views.StateDataAPI.as_view()),
+    path('<str:country>/<str:state>/zone', location_views.ZoneDataAPI.as_view()),
+    path('<str:country>/<str:state>/<str:zone>/district', location_views.DistrictDataAPI.as_view()),
 ]
