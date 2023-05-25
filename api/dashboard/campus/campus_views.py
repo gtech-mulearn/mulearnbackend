01d6bef01a8df9cdc9d35849233a9e6ae98519e2
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-05-25 23:36:36 +0530

    campus leaderboard completed

--- api/dashboard/campus/campus_views.py
+++ api/dashboard/campus/campus_views.py
@@ -16,15 +16,15 @@ class StudentDetailsAPI(APIView):
         user_id = JWTUtils.fetch_user_id(request)
         user_org_link = UserOrganizationLink.objects.filter(user_id=user_id,
                                                             org__org_type=OrganizationType.COLLEGE.value).first()
-        user_org_links = UserOrganizationLink.objects.filter(
-            org_id=user_org_link.org_id)
-        paginated_queryset = CommonUtils.get_paginated_queryset(
-            user_org_links, request, ['user__first_name'])
-        serializer = serializers.UserOrgSerializer(
-            paginated_queryset.get('queryset'), many=True)
+        user_org_links = UserOrganizationLink.objects.filter(org_id=user_org_link.org_id)
+        paginated_queryset = CommonUtils.get_paginated_queryset(user_org_links, request, ['user__first_name'])
+        serializer = serializers.UserOrgSerializer(paginated_queryset.get('queryset'), many=True).data
+        sorted_persons = sorted(serializer, key=lambda x: x['karma'], reverse=True)
+        for i, person in enumerate(sorted_persons):
+            person['rank'] = i + 1
 
-        return CustomResponse(response={"data": serializer.data,
-                                        "pagination": paginated_queryset.get('pagination')}).get_success_response()
+        return CustomResponse(response={"data": sorted_persons,
+                                        'pagination': paginated_queryset.get('pagination')}).get_success_response()
 
 
 class StudentDetailsCSVAPI(APIView):
