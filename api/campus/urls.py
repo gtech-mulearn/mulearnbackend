8c4cc878c6cb0d15df254288a1bb91c5c6038ea6
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-05-25 17:07:12 +0530

    change: campus dir to dashboard and make code more standard

--- api/campus/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from api.campus import campus_views
-
-urlpatterns = [
-    path("student-details/", campus_views.StudentDetails.as_view()),
-    path("student-details/csv/", campus_views.StudentDetailsCSV.as_view()),
-    path("campus-details/", campus_views.CampusDetails.as_view()),
-]
