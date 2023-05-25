67d3abca89e5759f08a0e694b913176769656faf
ZAFAR <muhammedzafar.mm@gmail.com>
2023-05-25 16:26:41 +0400

    [FEAT]! Add Role verification API

--- api/dashboard/user/dash_user_serializer.py
+++ api/dashboard/user/dash_user_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from db.user import User
+from db.user import User, UserRoleLink, Role
 
 
 class UserDashboardSerializer(serializers.ModelSerializer):
@@ -16,19 +16,28 @@ class UserDashboardSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "created_at", "total_karma"]
 
 
-# class UserVerificationSerializer(serializers.ModelSerializer):
-#     role_id = serializers.SerializerMethodField()
-#     role_title = serializers.SerializerMethodField()
+class UserVerificationSerializer(serializers.ModelSerializer):
+    first_name = serializers.ReadOnlyField(source="user.first_name")
+    last_name = serializers.ReadOnlyField(source="user.last_name")
+    user_id = serializers.ReadOnlyField(source="user.id")
+    discord_id = serializers.ReadOnlyField(source="user.discord_id")
+    mu_id = serializers.ReadOnlyField(source="user.mu_id")
+    role_title = serializers.SerializerMethodField()
 
-#     def get_role_id(self, user):
-#         role_ids_and_titles = UserRoleLink.objects.filter(user=user, verified=False).values_list('role_id', 'role__title')
-#         return [role_id for role_id, _ in role_ids_and_titles]
+    def get_role_title(self, obj):
+        roles = Role.objects.filter(id=obj.role_id)
+        return [role.title for role in roles]
 
-#     def get_role_title(self, user):
-#         role_ids_and_titles = UserRoleLink.objects.filter(user=user, verified=False).values_list('role_id', 'role__title')
-#         return [role_title for _, role_title in role_ids_and_titles]
-
-#     class Meta:
-#         model = User
-#         fields = ["user_id", "discord_id", "mu_id", "first_name", "last_name", "active", "exist_in_guild", "verified", "role_id", "role_title"]
-
+    class Meta:
+        model = UserRoleLink
+        fields = [
+            "id",
+            "user_id",
+            "discord_id",
+            "mu_id",
+            "first_name",
+            "last_name",
+            "verified",
+            "role_id",
+            "role_title",
+        ]
--- api/dashboard/user/dash_user_views.py
+++ api/dashboard/user/dash_user_views.py
@@ -1,17 +1,16 @@
 from django.db import IntegrityError
-from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import APIView
+from .dash_user_serializer import UserDashboardSerializer, UserVerificationSerializer
 
-from db.user import User
+from db.user import User, UserRoleLink
 from utils.permission import CustomizePermission, RoleRequired
 from utils.response import CustomResponse
 from utils.types import RoleType
 from utils.utils import CommonUtils
-from .dash_user_serializer import UserDashboardSerializer
 
 
 class UserAPI(APIView):
-    
     authentication_classes = [CustomizePermission]
 
     @RoleRequired(roles=[RoleType.ADMIN])
@@ -33,78 +32,102 @@ class UserAPI(APIView):
 
     @RoleRequired(roles=[RoleType.ADMIN])
     def patch(self, request, user_id):
-        user = get_object_or_404(User, id=user_id)
-        serializer = UserDashboardSerializer(user, data=request.data, partial=True)
+        try:
+            user = User.objects.get(id=user_id)
+        except ObjectDoesNotExist as e:
+            return CustomResponse(general_message=str(e)).get_failure_response()
 
+        serializer = UserDashboardSerializer(user, data=request.data, partial=True)
         if not serializer.is_valid():
             return CustomResponse(
-                response={"users": serializer.errors}
+                general_message=serializer.errors
             ).get_failure_response()
+
         try:
             serializer.save()
             return CustomResponse(
                 response={"users": serializer.data}
             ).get_success_response()
-
         except IntegrityError as e:
-            return CustomResponse(response={"users": str(e)}).get_failure_response()
+            return CustomResponse(
+                general_message="Database integrity error",
+            ).get_failure_response()
 
     @RoleRequired(roles=[RoleType.ADMIN])
     def delete(self, request, user_id):
-        user = get_object_or_404(User, id=user_id)
-        user.delete()
-        return CustomResponse().get_success_response()
+        try:
+            user = User.objects.get(id=user_id)
+            user.delete()
+            return CustomResponse(
+                general_message=["User deleted successfully"]
+            ).get_success_response()
+
+        except ObjectDoesNotExist as e:
+            return CustomResponse(general_message=str(e)).get_failure_response()
 
 
-# class UserVerificationAPI(APIView):
-#     authentication_classes = [CustomizePermission]
+class UserManagementCSV(APIView):
+    authentication_classes = [CustomizePermission]
 
-#     @RoleRequired(roles=[RoleType.ADMIN])
-#     def get(self, request):
-#         user_queryset = User.objects.all()
-#         queryset = CommonUtils.get_paginated_queryset(
-#             user_queryset,
-#             request,
-#             ["first_name", "last_name", "role_title"],
-#         )
-#         serializer = UserVerificationSerializer(queryset.get("queryset"), many=True)
+    @RoleRequired(roles=[RoleType.ADMIN, ])
+    def get(self, request):
+        user = User.objects.all()
+        user_serializer_data = UserDashboardSerializer(user, many=True).data
+        return CommonUtils.generate_csv(user_serializer_data, "User")
 
-#         return CustomResponse(
-#             response={
-#                 "users": serializer.data,
-#                 "pagination": queryset.get("pagination"),
-#             }
-#         ).get_success_response()
 
-#     # @RoleRequired(roles=[RoleType.ADMIN])
-#     def patch(self, request, user_id):
-#         user = get_object_or_404(User, id=user_id)
-#         role_id = request.data.get("roles", [])
-#         admin_id = User.objects.get(id="01ccd814-a33b-4f14-86e8-01319efb9a60")
-#         # admin_id = JWTUtils.fetch_user_id(request)
-        
-#         role = Role.objects.get(id=role_id)
-#         user_role_link = UserRoleLink(verified=True)
-        
-#         user_role_link.save()
+class UserVerificationAPI(APIView):
+    authentication_classes = [CustomizePermission]
 
-#         serializer = UserDashboardSerializer(user, data=request.data, partial=True)
+    @RoleRequired(roles=[RoleType.ADMIN])
+    def get(self, request):
+        user_queryset = UserRoleLink.objects.filter(verified=False)
+        queryset = CommonUtils.get_paginated_queryset(
+            user_queryset,
+            request,
+            ["first_name", "last_name", "role_title"],
+        )
+        serializer = UserVerificationSerializer(queryset.get("queryset"), many=True)
 
-#         if not serializer.is_valid():
-#             return CustomResponse(
-#                 response={"users": serializer.errors}
-#             ).get_failure_response()
-#         try:
-#             serializer.save()
-#             return CustomResponse(
-#                 response={"users": serializer.data}
-#             ).get_success_response()
+        return CustomResponse(
+            response={
+                "user_role_link": serializer.data,
+                "pagination": queryset.get("pagination"),
+            }
+        ).get_success_response()
 
-#         except IntegrityError as e:
-#             return CustomResponse(response={"users": str(e)}).get_failure_response()
+    @RoleRequired(roles=[RoleType.ADMIN])
+    def patch(self, request, link_id):
+        try:
+            user = UserRoleLink.objects.get(id=link_id)
+        except ObjectDoesNotExist as e:
+            return CustomResponse(general_message=str(e)).get_failure_response()
 
-#     # @RoleRequired(roles=[RoleType.ADMIN])
-#     def delete(self, request, user_id):
-#         user = get_object_or_404(User, id=user_id)
-#         user.delete()
-#         return CustomResponse().get_success_response()
+        serializer = UserVerificationSerializer(user, data=request.data, partial=True)
+
+        if not serializer.is_valid():
+            return CustomResponse(
+                response={"user_role_link": serializer.errors}
+            ).get_failure_response()
+        try:
+            serializer.save()
+            return CustomResponse(
+                response={"user_role_link": serializer.data}
+            ).get_success_response()
+
+        except IntegrityError as e:
+            return CustomResponse(
+                response={"user_role_link": str(e)}
+            ).get_failure_response()
+
+    @RoleRequired(roles=[RoleType.ADMIN])
+    def delete(self, request, link_id):
+        try:
+            link = UserRoleLink.objects.get(id=link_id)
+            link.delete()
+            return CustomResponse(
+                general_message=["Link deleted successfully"]
+            ).get_success_response()
+
+        except ObjectDoesNotExist as e:
+            return CustomResponse(general_message=str(e)).get_failure_response()
