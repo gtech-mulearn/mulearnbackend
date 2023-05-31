cb72e00ee99640257b7929d89001777cff51bad6
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-05-31 13:25:40 +0530

    UserProfileAPI

--- api/dashboard/profile/urls.py
+++ api/dashboard/profile/urls.py
@@ -3,8 +3,9 @@ from . import profile_view
 
 urlpatterns = [
     # path('add', portal_views.AddPortal.as_view()),
-    path('user-log/', profile_view.UserLogAPI.as_view()),
+    path('user-profile/', profile_view.UserProfileAPI.as_view()),
     path('edit-user-profile/', profile_view.EditUserDetailsAPI.as_view()),
+    path('user-log/', profile_view.UserLogAPI.as_view()),
     path('user-task-log/', profile_view.UserTaskLogAPI.as_view()),
     path('user-ig/', profile_view.UserInterestGroupAPI.as_view())
 ]
