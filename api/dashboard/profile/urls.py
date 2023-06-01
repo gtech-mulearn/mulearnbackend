e8d81521dd031132d8f249125fc9f78b7b091cdd
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-06-01 18:35:45 +0530

    UserSuggestionAPI

--- api/dashboard/profile/urls.py
+++ api/dashboard/profile/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('edit-user-profile/', profile_view.EditUserDetailsAPI.as_view()),
     path('user-log/', profile_view.UserLogAPI.as_view()),
     path('user-task-log/', profile_view.UserTaskLogAPI.as_view()),
-    path('user-ig/', profile_view.UserInterestGroupAPI.as_view())
+    path('user-ig/', profile_view.UserInterestGroupAPI.as_view()),
+    path('user-suggestion/', profile_view.UserSuggestionAPI.as_view())
 ]
