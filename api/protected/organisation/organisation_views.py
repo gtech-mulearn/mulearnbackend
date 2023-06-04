c836d3066b60f6be22f6ee5a3e01e7953a4df320
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-06-04 20:31:16 +0530

    restrive institute api completed

--- api/protected/organisation/organisation_views.py
+++ api/protected/organisation/organisation_views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 
 from db.organization import Organization, District
 from utils.response import CustomResponse
-from .serializer import OrganisationSerializer
+from .serializer import OrganisationSerializer, InstitutesRetrivalSerializer
 
 
 class GetInstitutionsAPI(APIView):
@@ -18,3 +18,15 @@ class GetInstitutionsAPI(APIView):
         organisations = Organization.objects.filter(org_type=organisation_type, district=district)
         organisation_serializer = OrganisationSerializer(organisations, many=True)
         return CustomResponse(response={'institutions': organisation_serializer.data}).get_success_response()
+
+
+class RetrieveInstitutesAPI(APIView):
+    def get(self, request, organisation_type, district_name):
+        protection_key = request.headers.get("protectionKey")
+        if not protection_key or not protection_key == config('PROTECTED_API_KEY'):
+            return CustomResponse(general_message="Invalid Key").get_failure_response()
+
+        district = District.objects.filter(name=district_name).first()
+        organisations = Organization.objects.filter(org_type=organisation_type, district=district)
+        serializer = InstitutesRetrivalSerializer(organisations, many=True)
+        return CustomResponse(response=serializer.data).get_success_response()
