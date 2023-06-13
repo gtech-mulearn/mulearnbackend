5d98308dfd60000a2727ff697ed3b1f2992c504e
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-06-13 16:15:10 +0530

    campus detials api issue fixed

--- api/dashboard/campus/campus_views.py
+++ api/dashboard/campus/campus_views.py
@@ -1,4 +1,3 @@
-from django.db.models import F, IntegerField, Case, When, Value
 from rest_framework.views import APIView
 
 from db.organization import UserOrganizationLink
@@ -7,26 +6,7 @@ from utils.response import CustomResponse
 from utils.types import OrganizationType, RoleType
 from utils.utils import CommonUtils
 from . import serializers
-from .serializers import UserOrgSerializer
 
-
-# class StudentDetailsAPI(APIView):
-#     authentication_classes = [CustomizePermission]
-#
-#     @RoleRequired(roles=[RoleType.CAMPUS_LEAD])
-#     def get(self, request):
-#         user_id = JWTUtils.fetch_user_id(request)
-#         user_org_link = UserOrganizationLink.objects.filter(user_id=user_id,
-#                                                             org__org_type=OrganizationType.COLLEGE.value).first()
-#         user_org_links = UserOrganizationLink.objects.filter(org_id=user_org_link.org_id)
-#         paginated_queryset = CommonUtils.get_paginated_queryset(user_org_links, request, ['user__first_name'])
-#         serializer = serializers.UserOrgSerializer(paginated_queryset.get('queryset'), many=True).data
-#         sorted_persons = sorted(serializer, key=lambda x: x['karma'], reverse=True)
-#         for i, person in enumerate(sorted_persons):
-#             person['rank'] = i + 1
-#
-#         return CustomResponse(response={"data": sorted_persons,
-#                                         'pagination': paginated_queryset.get('pagination')}).get_success_response()
 
 class StudentDetailsAPI(APIView):
     authentication_classes = [CustomizePermission]
@@ -34,34 +14,17 @@ class StudentDetailsAPI(APIView):
     @RoleRequired(roles=[RoleType.CAMPUS_LEAD])
     def get(self, request):
         user_id = JWTUtils.fetch_user_id(request)
-        user_org_links = UserOrganizationLink.objects.filter(
-            user_id=user_id,
-            org__org_type=OrganizationType.COLLEGE.value
-        ).values('org_id')
+        user_org_link = UserOrganizationLink.objects.filter(user_id=user_id,
+                                                            org__org_type=OrganizationType.COLLEGE.value).first()
+        user_org_links = UserOrganizationLink.objects.filter(org_id=user_org_link.org_id)
+        paginated_queryset = CommonUtils.get_paginated_queryset(user_org_links, request, ['user__first_name'])
+        serializer = serializers.UserOrgSerializer(paginated_queryset.get('queryset'), many=True).data
+        sorted_persons = sorted(serializer, key=lambda x: x['karma'], reverse=True)
+        for i, person in enumerate(sorted_persons):
+            person['rank'] = i + 1
 
-        queryset = UserOrganizationLink.objects.filter(
-            org_id__in=user_org_links,
-            user__total_karma_user__isnull=False
-        ).annotate(
-            fullname=F('user__fullname'),
-            muid=F('user__mu_id'),
-            karma=F('user__total_karma_user__karma'),
-            rank=Case(
-                When(user__total_karma_user__karma=F('karma'), then=Value(1)),
-                default=Value(0),
-                output_field=IntegerField(),
-            ),
-            level=F('user__userlvllink__level__name')
-        ).order_by('-karma')
-
-        serializer = UserOrgSerializer(queryset, many=True)
-        return CustomResponse(response=serializer.data).get_success_response()
-
-        # paginated_queryset = CommonUtils.get_paginated_queryset(queryset, request, ['fullname'])
-        # serializer = serializers.UserOrgSerializer(queryset, many=True)
-
-        # return CustomResponse(response={"data": serializer,
-        #                                 'pagination': paginated_queryset.get('pagination')}).get_success_response()
+        return CustomResponse(response={"data": sorted_persons,
+                                        'pagination': paginated_queryset.get('pagination')}).get_success_response()
 
 
 class StudentDetailsCSVAPI(APIView):
