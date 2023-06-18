bd84214e8c14d84d194500b57d68a2ab486adbed
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-06-19 02:39:01 +0530

    date time utils formated

--- utils/utils.py
+++ utils/utils.py
@@ -73,38 +73,15 @@ class DateTimeUtils:
     """
 
     @staticmethod
-    def get_current_utc_time(timezone: str = "Asia/Kolkata") -> datetime.datetime:
+    def get_current_utc_time() -> datetime.datetime:
         """
         Returns the current time in UTC.
 
         Returns:
             datetime.datetime: The current time in UTC.
         """
-        utc_now = datetime.datetime.utcnow()
-        local_tz = pytz.timezone(timezone)
-        local_now = utc_now.replace(tzinfo=pytz.utc).astimezone(local_tz)
+        local_now = datetime.datetime.now(pytz.timezone("UTC"))
         return DateTimeUtils.format_time(local_now)
-
-    @staticmethod
-    def utc_to_local(utc_dt: datetime.datetime, local_tz: str) -> str:
-        """
-        Converts a UTC datetime object to a datetime object in the specified timezone.
-
-        Args:
-            utc_dt (datetime.datetime): The datetime object to convert.
-            local_tz (str): The timezone to convert to.
-
-        Returns:
-            str: The converted datetime string in the format '%Y-%m-%d %I:%M:%S'.
-        """
-        utc_tz = pytz.timezone('UTC')
-        local_tz = pytz.timezone(local_tz)
-        utc_dt_naive = utc_dt.replace(tzinfo=None)
-        utc_dt_utc = utc_tz.localize(utc_dt_naive)
-        local_dt = utc_dt_utc.astimezone(local_tz)
-        date_obj = datetime.datetime.fromisoformat(str(local_dt))
-        formatted_date = date_obj.strftime("%Y-%m-%d %I:%M:%S")
-        return formatted_date
 
     @staticmethod
     def format_time(date_time: datetime.datetime) -> datetime.datetime:
@@ -121,18 +98,6 @@ class DateTimeUtils:
         formatted_time = date_time.strftime("%Y-%m-%d %H:%M:%S %z")
         return datetime.datetime.strptime(formatted_time, "%Y-%m-%d %H:%M:%S %z")
 
-    @staticmethod
-    def string_to_date_time(dt_str: str) -> datetime.datetime:
-        """
-        Converts a datetime string in the format '%Y-%m-%d %H:%M:%S' to a datetime object.
-
-        Args:
-            dt_str (str): The datetime string to convert.
-
-        Returns:
-            datetime.datetime: The converted datetime object.
-        """
-        return datetime.datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
 
 
 class _CustomHTTPHandler:
