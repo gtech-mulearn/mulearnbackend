1cdd69440f3a69fb189264ce04e5e756ec44448d
ZAFAR <muhammedzafar.mm@gmail.com>
2023-06-28 11:40:17 +0530

    [FEAT] Update integration table

--- db/integrations.py
+++ db/integrations.py
@@ -2,16 +2,23 @@ from django.db import models
 from db.user import User
 import uuid
 
-class KKEMAuthorization(models.Model):
-    id = models.CharField(max_length=36, primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='kkem_authorization_user')
-    dwms_id = models.CharField(max_length=36, unique=True)
+class Integration(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'integration'
+
+class IntegrationAuthorization(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    integration = models.ForeignKey(Integration, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='integration_authorization_user')
+    integration_value = models.CharField(max_length=36, unique=True)
     verified = models.BooleanField(default=False)
     updated_at = models.DateTimeField()
     created_at = models.DateTimeField()
-    
-    class Meta:
-        db_table = 'kkem_authorization'
 
-    def __str__(self):
-        return self.id
+    class Meta:
+        db_table = 'integration_authorization'
