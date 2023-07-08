71ce505b7217e60c96af9cbba26caab6de536f03
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-07-08 13:02:58 +0530

    TaskAPI Create and Update serialized

--- api/dashboard/task/dash_task_view.py
+++ api/dashboard/task/dash_task_view.py
@@ -9,7 +9,7 @@ from utils.permission import CustomizePermission, JWTUtils, role_required
 from utils.response import CustomResponse
 from utils.types import RoleType
 from utils.utils import CommonUtils, DateTimeUtils, ImportCSV
-from .dash_task_serializer import TaskListSerializer
+from .dash_task_serializer import TaskListSerializer, TaskUpdateSerializer, TaskCreateSerializer
 
 
 class TaskApi(APIView):
@@ -33,49 +33,78 @@ class TaskApi(APIView):
         return CustomResponse().paginated_response(data=task_serializer_data,
                                                    pagination=paginated_queryset.get('pagination'))
 
-    @role_required([RoleType.ADMIN.value, ])
-    def post(self, request):  # create
-        user_id = JWTUtils.fetch_user_id(request)
-        task_data = TaskList.objects.create(
-            id=uuid.uuid4(),
-            hashtag=request.data.get('hashtag'),
-            title=request.data.get('title'),
-            description=request.data.get('description'),
-            karma=request.data.get('karma'),
-            channel_id=request.data.get('channel_id'),
-            type_id=request.data.get('type_id'),
-            active=request.data.get('active'),
-            variable_karma=request.data.get('variable_karma'),
-            usage_count=request.data.get('usage_count'),
-            level_id=request.data.get('level_id'),
-            ig_id=request.data.get('ig_id'),
-            updated_by_id=user_id,
-            updated_at=DateTimeUtils.get_current_utc_time(),
-            created_by_id=user_id,
-            created_at=DateTimeUtils.get_current_utc_time())
-        serializer = TaskListSerializer(task_data)
-        return CustomResponse(response={"taskList": serializer.data}).get_success_response()
+    # @role_required([RoleType.ADMIN.value, ])
+    # def post(self, request):  # create
+    #     user_id = JWTUtils.fetch_user_id(request)
+    #     task_data = TaskList.objects.create(
+    #         id=uuid.uuid4(),
+    #         hashtag=request.data.get('hashtag'),
+    #         title=request.data.get('title'),
+    #         description=request.data.get('description'),
+    #         karma=request.data.get('karma'),
+    #         channel_id=request.data.get('channel_id'),
+    #         type_id=request.data.get('type_id'),
+    #         active=request.data.get('active'),
+    #         variable_karma=request.data.get('variable_karma'),
+    #         usage_count=request.data.get('usage_count'),
+    #         level_id=request.data.get('level_id'),
+    #         ig_id=request.data.get('ig_id'),
+    #         updated_by_id=user_id,
+    #         updated_at=DateTimeUtils.get_current_utc_time(),
+    #         created_by_id=user_id,
+    #         created_at=DateTimeUtils.get_current_utc_time())
+    #     serializer = TaskListSerializer(task_data)
+    #     return CustomResponse(response={"taskList": serializer.data}).get_success_response()
+
+    # @role_required([RoleType.ADMIN.value, ])
+    # def put(self, request, pk):  # edit
+    #     user_id = JWTUtils.fetch_user_id(request)
+    #     taskData = TaskList.objects.filter(id=pk).first()
+    #     fields_to_update = ["hashtag",
+    #                         "title",
+    #                         "karma",
+    #                         "active",
+    #                         "variable_karma",
+    #                         "usage_count"]
+    #     for field in fields_to_update:
+    #         if field in request.data:
+    #             setattr(taskData, field, request.data[field])
+    #     taskData.updated_by = User.objects.filter(id=user_id).first()
+    #     taskData.updated_at = DateTimeUtils.get_current_utc_time()
+    #     taskData.save()
+    #     serializer = TaskListSerializer(taskData)
+    #     return CustomResponse(
+    #         response={"taskList": serializer.data}
+    #     ).get_success_response()
 
     @role_required([RoleType.ADMIN.value, ])
-    def put(self, request, pk):  # edit
+    def post(self, request):  # create
+
         user_id = JWTUtils.fetch_user_id(request)
-        taskData = TaskList.objects.filter(id=pk).first()
-        fields_to_update = ["hashtag",
-                            "title",
-                            "karma",
-                            "active",
-                            "variable_karma",
-                            "usage_count"]
-        for field in fields_to_update:
-            if field in request.data:
-                setattr(taskData, field, request.data[field])
-        taskData.updated_by = User.objects.filter(id=user_id).first()
-        taskData.updated_at = DateTimeUtils.get_current_utc_time()
-        taskData.save()
-        serializer = TaskListSerializer(taskData)
-        return CustomResponse(
-            response={"taskList": serializer.data}
-        ).get_success_response()
+
+        serializer = TaskCreateSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(general_message='Task Created Successfully').get_success_response()
+
+        return CustomResponse(message=serializer.errors).get_failure_response()
+
+    @role_required([RoleType.ADMIN.value, ])
+    def put(self, request, task_id):  # edit
+
+        user_id = JWTUtils.fetch_user_id(request)
+        task_list = TaskList.objects.filter(id=task_id).first()
+
+        if TaskList is None:
+            return CustomResponse(general_message='Invalid task id').get_failure_response()
+
+        serializer = TaskUpdateSerializer(task_list, data=request.data,
+                                          context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(general_message='Task Edited Successfully').get_success_response()
+
+        return CustomResponse(message=serializer.errors).get_failure_response()
 
     @role_required([RoleType.ADMIN.value, ])
     def patch(self, request, pk):  # delete
