0f79c4c8d37edfee8faea9b8e209ad372da16541
Aashish <8xdeadshot@gmail.com>
2023-07-16 23:35:10 +0530

    feat(LearningCircle): Added new endpoint for each learning circle , data listing of specific learning circle

--- api/dashboard/lc/urls.py
+++ api/dashboard/lc/urls.py
@@ -4,5 +4,6 @@ from . import dash_lc_view
 urlpatterns = [
     path('', dash_lc_view.LearningCircleListApi.as_view()),
     path('join/', dash_lc_view.LearningCircleAPI.as_view()),
-    path('create/', dash_lc_view.LearningCircleAPI.as_view())
+    path('create/', dash_lc_view.LearningCircleAPI.as_view()),
+    path('<str:circle_id>/', dash_lc_view.LearningCircleHomeApi.as_view())
 ]
