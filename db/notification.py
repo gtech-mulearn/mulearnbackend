89317df04662e914cbf6efb738f76ddb9b28032b
Suryan S <suryannasa@gmail.com>
2023-07-27 17:01:19 +0530

    fix: minor update on the table definition

--- db/notification.py
+++ db/notification.py
@@ -1,14 +1,15 @@
 from django.db import models
 from db.user import User
+import uuid
 
 
 class Notification(models.Model):
-    id = models.CharField(max_length=36, primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    button = models.CharField(max_length=10)
-    url = models.CharField(max_length=100)
+    title = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=200, blank=False)
+    button = models.CharField(max_length=10, blank=True, null=True)
+    url = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, db_column='created_by', related_name='created_notifications')
