c589adc38fc45844c7a4febba5f1068ea30b0c9a
ZAFAR <muhammedzafar.mm@gmail.com>
2023-07-28 16:45:51 +0530

    [FEAT] API Wrapper for KKEM

--- api/integrations/kkem/urls.py
+++ api/integrations/kkem/urls.py
@@ -6,6 +6,7 @@ from . import kkem_views
 urlpatterns = [ 
     path('login/', kkem_views.KKEMIntegrationLogin.as_view(), name="login-auth"),
     path('authorization/', kkem_views.KKEMAuthorizationAPI.as_view(), name="create-auth"),
+    path('user/<str:jsid>/', kkem_views.KKEMdetailsFetchAPI.as_view(), name="get-details"),
     path('authorization/<str:token>/', kkem_views.KKEMAuthorizationAPI.as_view(), name="verify-auth"),
     path('users/', kkem_views.KKEMBulkKarmaAPI.as_view(), name="list-user"),
     path('users/<str:mu_id>/', kkem_views.KKEMIndividualKarmaAPI.as_view(), name="get-user"),
