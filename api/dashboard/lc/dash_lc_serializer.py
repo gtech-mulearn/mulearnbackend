312730755163d8c02f022071d8d7c4000dd613d6
Aashish <8xdeadshot@gmail.com>
2023-08-05 21:51:12 +0530

    fix(LearningCircle): typo in day editing

--- api/dashboard/lc/dash_lc_serializer.py
+++ api/dashboard/lc/dash_lc_serializer.py
@@ -237,7 +237,7 @@ class LearningCircleMeetSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.meet_time = validated_data.get('meet_time')
         instance.meet_place = validated_data.get('meet_place')
-        instance.day = validated_data.get('date')
+        instance.day = validated_data.get('day')
         instance.updated_at = DateTimeUtils.get_current_utc_time()
         instance.save()
         return instance
