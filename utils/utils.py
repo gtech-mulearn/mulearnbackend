f6e6a872a3932776a529cb535152514b987a854b
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-08-12 22:24:43 +0530

    send mail fix

--- utils/utils.py
+++ utils/utils.py
@@ -184,4 +184,5 @@ def send_dashboard_mail(user_data: dict, subject: str, address: list[str]):
         from_email=email_host_user,
         recipient_list=[user_data["email"]],
         html_message=email_content,
+        fail_silently=False,
     )
