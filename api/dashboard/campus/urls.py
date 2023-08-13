44b9f33cfffe444263d6f4d1ccfd78e97c30fdff
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-08-14 01:08:20 +0530

    removed org_type from Campus Api

--- api/dashboard/campus/urls.py
+++ api/dashboard/campus/urls.py
@@ -5,7 +5,7 @@ from . import campus_views
 urlpatterns = [
     path("campus-details/", campus_views.CampusDetailsAPI.as_view()),
     path("student-level/", campus_views.CampusStudentInEachLevelAPI.as_view()),
-    path("student-details/<str:org_type>/", campus_views.CampusStudentDetailsAPI.as_view()),
-    path("student-details/<str:org_type>/csv/", campus_views.CampusStudentDetailsCSVAPI.as_view()),
+    path("student-details/", campus_views.CampusStudentDetailsAPI.as_view()),
+    path("student-details/csv/", campus_views.CampusStudentDetailsCSVAPI.as_view()),
     path("weekly-karma/", campus_views.WeeklyKarmaAPI.as_view()),
 ]
