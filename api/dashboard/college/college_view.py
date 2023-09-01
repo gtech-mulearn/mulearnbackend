2c2f85f504b48403c67c01bc3a766676d288ba7a
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-09-01 21:27:06 +0530

    minor fix in college

--- api/dashboard/college/college_view.py
+++ api/dashboard/college/college_view.py
@@ -1,9 +1,8 @@
-from db.organization import College, Organization
-from utils.types import OrganizationType
+from rest_framework.views import APIView
+
+from db.organization import College
 from utils.permission import JWTUtils
 from utils.response import CustomResponse
-from decouple import config
-from rest_framework.views import APIView
 from .serializer import CollegeCreateDeleteSerializer, CollegeListSerializer, CollegeEditSerializer
 
 
@@ -22,14 +21,14 @@ class CollegeApi(APIView):
         return CustomResponse(message=serializer.errors).get_failure_response()
 
     def put(self, request):
-        if college := College.objects.filter(org_id=request.data.get('org_id')).first():
+        if college := College.objects.filter(org__id=request.data.get('org_id')).first():
             CollegeCreateDeleteSerializer().destroy(college)
             return CustomResponse(general_message='College succesfully deleted').get_success_response()
         return CustomResponse(general_message='Invalid college').get_failure_response()
 
     def patch(self, request):
         user_id = JWTUtils.fetch_user_id(request)
-        college = College.objects.filter(org_id=request.data.get('org_id')).first()
+        college = College.objects.filter(org__id=request.data.get('org_id')).first()
         serializer = CollegeEditSerializer(college, data=request.data, context={'user_id': user_id})
         if serializer.is_valid():
             serializer.save()
