bd746f7ab105764f47297554d5723e1de75da7a4
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-09-04 20:10:26 +0530

    learningcircle list api

--- api/dashboard/lc/urls.py
+++ api/dashboard/lc/urls.py
@@ -4,13 +4,14 @@ from . import dash_lc_view
 
 urlpatterns = [
     path('', dash_lc_view.LearningCircleListApi.as_view()),
+    path('list-members/<str:circle_id>/', dash_lc_view.LearningCircleListMembersApi.as_view()),
     path('list/', dash_lc_view.LearningCircleMainApi.as_view()),
     path('data/', dash_lc_view.LearningCircleDataAPI.as_view()),
     path('create/', dash_lc_view.LearningCircleAPI.as_view()),
     path('meet/<str:circle_id>/', dash_lc_view.LearningCircleMeetAPI.as_view()),
     path('join/<str:circle_id>/', dash_lc_view.LearningCircleJoinApi.as_view()),
     path('<str:circle_id>/', dash_lc_view.LearningCircleHomeApi.as_view()),
-    # path('<str:circle_id>/<str:member_id>/', dash_lc_view.LearningCircleHomeApi.as_view()),
-    path('list-members/<str:circle_id>/', dash_lc_view.LearningCircleListMembersApi.as_view()),
+    path('<str:circle_id>/<str:member_id>/', dash_lc_view.LearningCircleHomeApi.as_view()),
+
 
 ]
