653dfc06931c7e0bd846a748580d84a6b48275c2
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-09-14 00:14:35 +0530

    optim(organization apis): InstitutionCSV, InstitutionsAPI, GetInstitutionsAPI

--- api/dashboard/organisation/urls.py
+++ api/dashboard/organisation/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('institutes/info/all_inst/', organisation_views.InstitutionsAPI.as_view()),
     path('institutes/info/<str:org_code>/', organisation_views.InstitutionsAPI.as_view()),
     path('institutes/show/<str:organisation_type>/', organisation_views.GetInstitutionsAPI.as_view()),
+    path('institutes/show/<str:organisation_type>/<str:district_id>/', organisation_views.GetInstitutionsAPI.as_view()),
     path('institutes/org/affiliation/', organisation_views.AffiliationAPI.as_view()),
     path('institutes/names/<str:organisation_type>/', organisation_views.GetInstitutionsNamesAPI.as_view()),
 
