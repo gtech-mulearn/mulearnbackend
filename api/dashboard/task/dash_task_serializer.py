c662fef052b6b98fa65988859de57de06f96aa1e
lordgrim18 <jeromjomanthara@gmail.com>
2023-09-16 01:40:30 +0530

    fix(task): fixed ImportTaskListCSV API

--- api/dashboard/task/dash_task_serializer.py
+++ api/dashboard/task/dash_task_serializer.py
@@ -119,6 +119,20 @@ class TaskUpdateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Enter a valid interest group id")
         return ig
 
+class TaskImportSerializer(serializers.ModelSerializer):
+    created_by_id = serializers.CharField(required=True, allow_null=False)
+    updated_by_id = serializers.CharField(required=True, allow_null=False)
+    channel_id = serializers.CharField(required=False, allow_null=True)
+    type_id = serializers.CharField(required=False, allow_null=True)
+    org_id = serializers.CharField(required=False, allow_null=True)
+    level_id = serializers.CharField(required=False, allow_null=True)
+    ig_id = serializers.CharField(required=False, allow_null=True)
+
+    class Meta:
+        model = TaskList
+        fields = ("id", "hashtag", "title", "description", "karma", "channel_id", "type_id", "org_id",
+                  "level_id", "ig_id", "active", "variable_karma", "usage_count", "created_by_id", "updated_by_id", "created_at", "updated_at")
+
 
 class ChannelDropdownSerializer(serializers.ModelSerializer):
     class Meta:
--- api/dashboard/task/dash_task_view.py
+++ api/dashboard/task/dash_task_view.py
@@ -12,7 +12,7 @@ from utils.types import RoleType
 from utils.utils import CommonUtils, DateTimeUtils, ImportCSV
 from .dash_task_serializer import TaskListSerializer, TaskUpdateSerializer, TaskCreateSerializer, \
     ChannelDropdownSerializer, IGDropdownSerializer, OrganizationDropdownSerialize, LevelDropdownSerialize, \
-    TaskTypeDropdownSerializer
+    TaskTypeDropdownSerializer, TaskImportSerializer
 
 
 class TaskApi(APIView):
@@ -105,7 +105,8 @@ class ImportTaskListCSV(APIView):
         except KeyError:
             return CustomResponse(general_message={'File not found.'}).get_failure_response()
 
-        excel_data = ImportCSV.read_excel_file(file_obj)
+        excel_data = ImportCSV()
+        excel_data = excel_data.read_excel_file(file_obj)
         if not excel_data:
             return CustomResponse(general_message={'Empty csv file.'}).get_failure_response()
 
@@ -119,62 +120,98 @@ class ImportTaskListCSV(APIView):
         valid_rows = []
         error_rows = []
 
+        channels_to_fetch = set()
+        task_types_to_fetch = set()
+        levels_to_fetch = set()
+        igs_to_fetch = set()
+        orgs_to_fetch = set()
+
         for row in excel_data[1:]:
             hashtag = row.get('hashtag')
-            title = row.get('title')
-            description = row.get('description')
-            karma = row.get('karma')
-            usage_count = row.get('usage_count')
-            variable_karma = row.get('variable_karma')
+            level = row.get('level')
+            channel = row.get('channel')
+            task_type = row.get('type')
+            ig = row.get('ig')
+            org = row.get('org')
+
+            channels_to_fetch.add(channel)
+            task_types_to_fetch.add(task_type)
+            levels_to_fetch.add(level)
+            igs_to_fetch.add(ig)
+            orgs_to_fetch.add(org)
+
+        channels = Channel.objects.filter(name__in=channels_to_fetch).values('id', 'name')
+        task_types = TaskType.objects.filter(title__in=task_types_to_fetch).values('id', 'title')
+        levels = Level.objects.filter(name__in=levels_to_fetch).values('id', 'name')
+        igs = InterestGroup.objects.filter(name__in=igs_to_fetch).values('id', 'name')
+        orgs = Organization.objects.filter(code__in=orgs_to_fetch).values('id', 'code')
+
+        channels_dict = {channel['name']: channel['id'] for channel in channels}
+        task_types_dict = {task_type['title']: task_type['id'] for task_type in task_types}
+        levels_dict = {level['name']: level['id'] for level in levels}
+        igs_dict = {ig['name']: ig['id'] for ig in igs}
+        orgs_dict = {org['code']: org['id'] for org in orgs}
+
+        for row in excel_data[1:]:
+            hashtag = row.get('hashtag')
             level = row.pop('level')
             channel = row.pop('channel')
             task_type = row.pop('type')
             ig = row.pop('ig')
             org = row.pop('org')
 
-            channel_obj = Channel.objects.filter(name=channel).first()
-            task_type_obj = TaskType.objects.filter(title=task_type).first()
-            level_obj = Level.objects.filter(name=level).first() if level is not None else None
-            ig_obj = InterestGroup.objects.filter(name=ig).first() if ig is not None else None
-            org_obj = Organization.objects.filter(code=org).first() if org is not None else None
+            channel_id = channels_dict.get(channel)
+            task_type_id = task_types_dict.get(task_type)
+            level_id = levels_dict.get(level) if level is not None else None
+            ig_id = igs_dict.get(ig) if ig is not None else None
+            org_id = orgs_dict.get(org) if org is not None else None
+
+            # channel_obj = Channel.objects.filter(name=channel).first()
+            # task_type_obj = TaskType.objects.filter(title=task_type).first()
+            # level_obj = Level.objects.filter(name=level).first() if level is not None else None
+            # ig_obj = InterestGroup.objects.filter(name=ig).first() if ig is not None else None
+            # org_obj = Organization.objects.filter(code=org).first() if org is not None else None
 
             if TaskList.objects.filter(hashtag=hashtag).exists():
                 row['error'] = f"Hashtag already exists: {hashtag}"
                 error_rows.append(row)
-            elif not channel_obj:
+            elif not channel_id:
                 row['error'] = f"Invalid channel ID: {channel}"
                 error_rows.append(row)
-            elif not task_type_obj:
+            elif not task_type_id:
                 row['error'] = f"Invalid task type ID: {task_type}"
                 error_rows.append(row)
-            elif level and not level_obj:
+            elif level and not level_id:
                 row['error'] = f"Invalid level ID: {level}"
                 error_rows.append(row)
-            elif ig and not ig_obj:
+            elif ig and not ig_id:
                 row['error'] = f"Invalid interest group ID: {ig}"
                 error_rows.append(row)
-            elif org and not org_obj:
+            elif org and not org_id:
                 row['error'] = f"Invalid organization ID: {org}"
                 error_rows.append(row)
             else:
-                user_id = 1
-                row['id'] = uuid.uuid4()
+                user_id = JWTUtils.fetch_user_id(request)
+                row['id'] = str(uuid.uuid4())
                 row['updated_by_id'] = user_id
-                row['updated_at'] = str(DateTimeUtils.get_current_utc_time())
+                row['updated_at'] = DateTimeUtils.get_current_utc_time()
                 row['created_by_id'] = user_id
-                row['created_at'] = str(DateTimeUtils.get_current_utc_time())
+                row['created_at'] = DateTimeUtils.get_current_utc_time()
                 row['active'] = True
-                row['channel_id'] = channel_obj.id if channel_obj else None
-                row['type_id'] = task_type_obj.id if task_type_obj else None
-                row['level_id'] = level_obj.id if level_obj else None
-                row['ig_id'] = ig_obj.id if ig_obj else None
-                row['org_id'] = org_obj.id if ig_obj else None
-                TaskList.objects.create(**row)
-                print(row)
+                row['channel_id'] = channel_id
+                row['type_id'] = task_type_id
+                row['level_id'] = level_id if level_id else None
+                row['ig_id'] = ig_id if ig_id else None
+                row['org_id'] = org_id if org_id else None
                 valid_rows.append(row)
 
-        return CustomResponse(response={"Success": valid_rows, "Failed": error_rows}).get_success_response()
+        task_list_serializer = TaskImportSerializer(data=valid_rows, many=True)
+        if task_list_serializer.is_valid():
+            task_list_serializer.save()
+        else:
+            error_rows.append(task_list_serializer.errors)
 
+        return CustomResponse(response={"Success": task_list_serializer.data, "Failed": error_rows}).get_success_response()
 
 class TaskGetAPI(APIView):
     authentication_classes = [CustomizePermission]
