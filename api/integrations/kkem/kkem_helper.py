4b35f194dd01f0f7221fb161e9b9d63c914ff83c
Shaheen Hyder K <shaheenhyderk@gmail.com>
2023-09-19 12:33:17 +0530

    Update kkem_helper.py

--- api/integrations/kkem/kkem_helper.py
+++ api/integrations/kkem/kkem_helper.py
@@ -15,7 +15,7 @@ from utils.utils import send_template_mail
 
 def send_data_to_kkem(kkem_link):
     response = requests.post(
-        url="https://stagging.knowledgemission.kerala.gov.in/MuLearn/api/update/muLearnId",
+        url="https://knowledgemission.kerala.gov.in/MuLearn/api/update/muLearnId",
         data=json.dumps(
             {
                 "mu_id": kkem_link.user.mu_id,
