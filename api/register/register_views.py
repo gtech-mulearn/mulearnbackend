802eb3551ab2e521d17184644015da4775d9a3d8
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-09-20 13:19:56 +0530

    [FEAT] New register API

--- api/register/register_views.py
+++ api/register/register_views.py
@@ -1,6 +1,8 @@
 import decouple
 import requests
 from django.db.models import Q
+from django.db import transaction
+
 from rest_framework.views import APIView
 from api.register.register_helper import generate_mu_id
 
@@ -17,55 +19,120 @@ class RegisterAPI(APIView):
     def post(self, request):
         try:
             request_data = request.data
-            
-            request_data["user"]["mu_id"] = generate_mu_id(
-                request_data["user"]["first_name"], request_data["user"]["last_name"]
+            request_data["mu_id"] = generate_mu_id(
+                request_data["first_name"],
+                request_data["last_name"],
             )
-            if "dwms" in request_data:
-                request_data["dwms"]["emailOrMuid"] = request_data["user"]["mu_id"]
-                request_data["dwms"]["verified"] = True
-            
-            serialized_user = serializers.RegisterNewSerializer(data=request_data)
+
+            serialized_user = serializers.UserSerializer(
+                data=request_data, context={"request_data": request_data}
+            )
 
             if not serialized_user.is_valid():
                 return CustomResponse(
                     general_message=serialized_user.errors
                 ).get_failure_response()
 
-            user_obj, password = serialized_user.save()
-            auth_domain = decouple.config("AUTH_DOMAIN")
+            serialized_user.save()
+            user_data = serialized_user.data
+
+            AUTH_DOMAIN = decouple.config("AUTH_DOMAIN")
+
             response = requests.post(
-                f"{auth_domain}/api/v1/auth/user-authentication/",
-                data={"emailOrMuid": user_obj.mu_id, "password": password},
+                f"{AUTH_DOMAIN}/api/v1/auth/user-authentication/",
+                data={
+                    "emailOrMuid": user_data["mu_id"],
+                    "password": request_data["password"],
+                },
             )
             response = response.json()
+
             if response.get("statusCode") != 200:
                 return CustomResponse(
                     message=response.get("message")
                 ).get_failure_response()
 
-            res_data = response.get("response")
-            access_token = res_data.get("accessToken")
-            refresh_token = res_data.get("refreshToken")
-
-            response = {
-                "accessToken": access_token,
-                "refreshToken": refresh_token,
-            }
-
-            response_data = serializers.UserDetailSerializer(user_obj, many=False).data
-
             send_template_mail(
-                context=response_data,
+                context=user_data,
                 subject="YOUR TICKET TO µFAM IS HERE!",
                 address=["user_registration.html"],
             )
 
-            response["data"] = response_data
-
-            return CustomResponse(response=response).get_success_response()
+            return CustomResponse(
+                general_message="User successfully registered",
+                response=response.get("response"),
+            ).get_success_response()
         except Exception as e:
             return CustomResponse(general_message=str(e)).get_failure_response()
+
+
+# class RegisterAPI(APIView):
+#     def post(self, request):
+#         try:
+#             user = None
+#             request_data = request.data
+#             request_data["user"]["mu_id"] = generate_mu_id(
+#                 request_data["user"]["first_name"],
+#                 request_data["user"]["last_name"],
+#             )
+
+#             serialized_user = serializers.UserSerializer(
+#                 data=request_data["user"], context={"request_data": request_data}
+#             )
+
+#             if not serialized_user.is_valid():
+#                 return CustomResponse(
+#                     general_message=serialized_user.errors
+#                 ).get_failure_response()
+
+#             user = serialized_user.save()
+#             user_data = serialized_user.data
+
+#             with transaction.atomic():
+#                 serialized_user_info = serializers.RegisterNewSerializer(data=user_data)
+
+#                 if not serialized_user_info.is_valid():
+#                     return CustomResponse(
+#                         general_message=serialized_user_info.errors
+#                     ).get_failure_response()
+
+#                 serialized_user_info.save()
+
+#             user_obj, password = serialized_user.save()
+#             auth_domain = decouple.config("AUTH_DOMAIN")
+#             response = requests.post(
+#                 f"{auth_domain}/api/v1/auth/user-authentication/",
+#                 data={"emailOrMuid": user_obj.mu_id, "password": password},
+#             )
+#             response = response.json()
+#             if response.get("statusCode") != 200:
+#                 return CustomResponse(
+#                     message=response.get("message")
+#                 ).get_failure_response()
+
+#             res_data = response.get("response")
+#             access_token = res_data.get("accessToken")
+#             refresh_token = res_data.get("refreshToken")
+
+#             response = {
+#                 "accessToken": access_token,
+#                 "refreshToken": refresh_token,
+#             }
+
+#             response_data = serializers.UserDetailSerializer(user_obj, many=False).data
+
+#             send_template_mail(
+#                 context=response_data,
+#                 subject="YOUR TICKET TO µFAM IS HERE!",
+#                 address=["user_registration.html"],
+#             )
+
+#             response["data"] = response_data
+
+#             return CustomResponse(response=response).get_success_response()
+#         except Exception as e:
+#             user.delete() if user else None
+#             return CustomResponse(general_message=str(e)).get_failure_response()
 
 
 class LearningCircleUserViewAPI(APIView):
