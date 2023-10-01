968e31f0aa9f5800f40dc685be37599d486810cd
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-10-01 16:04:10 +0530

    [PATCH] student leaderboard only students

---
+++ api/leaderboard/leaderboard_view.py
@@ -0,0 +1,91 @@
+from django.db.models import Sum, F, Value
+from django.db.models.functions import Concat
+from rest_framework.views import APIView
+
+from db.organization import Organization
+from db.user import User
+from utils.response import CustomResponse
+from utils.types import OrganizationType, RoleType
+from utils.utils import DateTimeUtils
+
+
+class StudentsLeaderboard(APIView):
+    def get(self, request):
+        students_leaderboard = (
+            User.objects.filter(
+                user_organization_link_user__org__org_type=OrganizationType.COLLEGE.value,
+                user_role_link_user__role__title=RoleType.STUDENT.value
+            )
+            .values(
+                total_karma=F("wallet_user__karma"),
+                full_name=Concat(F("first_name"), Value(" "), F("last_name")),
+                institution=F("user_organization_link_user__org__title"),
+            )
+            .order_by("-total_karma")[:20]
+        )
+        return CustomResponse(response=students_leaderboard).get_success_response()
+
+
+class StudentsMonthlyLeaderboard(APIView):
+    def get(self, request):
+        start_date, end_date = DateTimeUtils.get_start_and_end_of_previous_month()
+
+        student_monthly_leaderboard = (
+            User.objects.filter(
+                user_organization_link_user__org__org_type=OrganizationType.COLLEGE.value,
+                karma_activity_log_user__created_at__range=(start_date, end_date),
+            )
+            .annotate(karma=Sum("karma_activity_log_user__karma"))
+            .values(
+                "id",
+                full_name=Concat(F("first_name"), Value(" "), F("last_name")),
+                total_karma=F("karma"),
+            )
+            .order_by("-total_karma")[:20]
+        )
+
+        return CustomResponse(
+            response=student_monthly_leaderboard
+        ).get_success_response()
+
+
+class CollegeLeaderboard(APIView):
+    def get(self, request):
+        college_leaderboard = (
+            Organization.objects.filter(org_type=OrganizationType.COLLEGE.value)
+            .values(
+                "code",
+                instituion=F("title"),
+                total_karma=Sum("user_organization_link_org__user__wallet_user__karma"),
+            )
+            .order_by("-total_karma")[:20]
+        )
+        
+
+        return CustomResponse(response=college_leaderboard).get_success_response()
+
+
+class CollegeMonthlyLeaderboard(APIView):
+    def get(self, request):
+        start_date, end_date = DateTimeUtils.get_start_and_end_of_previous_month()
+
+        college_monthly_leaderboard = (
+            Organization.objects.filter(
+                user_organization_link_org__user__karma_activity_log_user__created_at__range=(
+                    start_date,
+                    end_date,
+                ),
+                org_type=OrganizationType.COLLEGE.value,
+            )
+            .annotate(
+                karma=Sum(
+                    "user_organization_link_org__user__karma_activity_log_user__karma"
+                )
+            )
+            .values("code", institution=F("title"), total_karma=F("karma"))
+            .order_by("-total_karma")[:20]
+        )
+
+        return CustomResponse(
+            response=college_monthly_leaderboard
+        ).get_success_response()
---
+++ api/leaderboard/serializers.py
@@ -0,0 +1,76 @@
+from datetime import timedelta
+
+from django.db.models import Sum
+from rest_framework import serializers
+
+from db.organization import UserOrganizationLink
+from db.task import KarmaActivityLog
+from utils.types import OrganizationType
+from utils.utils import DateTimeUtils
+
+
+class CampusDetailsSerializer(serializers.ModelSerializer):
+    college_name = serializers.ReadOnlyField(source="org.title")
+    campus_code = serializers.ReadOnlyField(source="org.code")
+    campus_zone = serializers.ReadOnlyField(source="org.district.zone.name")
+    campus_lead = serializers.ReadOnlyField(source="user.fullname")
+    total_karma = serializers.SerializerMethodField()
+    total_members = serializers.SerializerMethodField()
+    active_members = serializers.SerializerMethodField()
+    rank = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserOrganizationLink
+        fields = [
+            "college_name",
+            "campus_lead",
+            "campus_code",
+            "campus_zone",
+            "total_karma",
+            "total_members",
+            "active_members",
+            "rank",
+        ]
+
+    def get_total_members(self, obj):
+        return obj.org.user_organization_link_org.count()
+
+    def get_active_members(self, obj):
+        last_month = DateTimeUtils.get_current_utc_time() - timedelta(days=30)
+        return obj.org.user_organization_link_org.filter(
+            verified=True,
+            user__active=True,
+            user__wallet_user__isnull=False,
+            user__wallet_user__created_at__gte=last_month,
+        ).count()
+
+    def get_total_karma(self, obj):
+        return (
+                obj.org.user_organization_link_org.filter(
+                    org__org_type=OrganizationType.COLLEGE.value,
+                    verified=True,
+                    user__wallet_user__isnull=False,
+                ).aggregate(total_karma=Sum("user__wallet_user__karma"))["total_karma"]
+                or 0
+        )
+
+    def get_rank(self, obj):
+        org_karma_dict = (
+            UserOrganizationLink.objects.all()
+            .values("org")
+            .annotate(total_karma=Sum("user__wallet_user__karma"))
+        )
+
+        rank_dict = {
+            data["org"]: data["total_karma"] if data["total_karma"] is not None else 0
+            for data in org_karma_dict
+        }
+
+        sorted_rank_dict = dict(
+            sorted(rank_dict.items(), key=lambda x: x[1], reverse=True)
+        )
+
+        if obj.org.id in sorted_rank_dict:
+            keys_list = list(sorted_rank_dict.keys())
+            position = keys_list.index(obj.org.id)
+            return position + 1
