fdb5fc2a30b478b7b3058b0118a2c77ae5a75a17
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-10-05 00:31:50 +0530

    [FEAT] Task dashboard optimisation, restructure

--- api/dashboard/task/dash_task_serializer.py
+++ api/dashboard/task/dash_task_serializer.py
@@ -1,4 +1,3 @@
-
 from rest_framework import serializers
 
 from db.task import TaskList
@@ -59,3 +58,36 @@ class TaskModifySerializer(serializers.ModelSerializer):
             "updated_by",
             "created_by",
         )
+
+
+class TaskImportSerializer(serializers.ModelSerializer):
+    created_by_id = serializers.CharField(required=True, allow_null=False)
+    updated_by_id = serializers.CharField(required=True, allow_null=False)
+    channel_id = serializers.CharField(required=False, allow_null=True)
+    type_id = serializers.CharField(required=False, allow_null=True)
+    org_id = serializers.CharField(required=False, allow_null=True)
+    level_id = serializers.CharField(required=False, allow_null=True)
+    ig_id = serializers.CharField(required=False, allow_null=True)
+
+    class Meta:
+        model = TaskList
+        fields = [
+            "id",
+            "hashtag",
+            "title",
+            "description",
+            "karma",
+            "channel_id",
+            "type_id",
+            "org_id",
+            "event",
+            "level_id",
+            "ig_id",
+            "active",
+            "variable_karma",
+            "usage_count",
+            "created_by_id",
+            "updated_by_id",
+            "created_at",
+            "updated_at",
+        ]
