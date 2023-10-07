ae45468e7e8fd7f11b5e5991d83b57312c2664d5
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-10-07 12:09:53 +0530

    profile org district id

--- api/dashboard/profile/profile_serializer.py
+++ api/dashboard/profile/profile_serializer.py
@@ -1,7 +1,7 @@
 import uuid
 
 from django.db import transaction
-from django.db.models import F, Prefetch, Q, Sum
+from django.db.models import F, Q, Sum
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -34,6 +34,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     level = serializers.SerializerMethodField()
     interest_groups = serializers.SerializerMethodField()
     is_public = serializers.SerializerMethodField()
+    org_district_id = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -46,6 +47,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "muid",
             "roles",
             "college_id",
+            "org_district_id"
             "college_code",
             "karma",
             "rank",
@@ -67,6 +69,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "roles") else OrganizationType.COLLEGE.value
         user_org_link = obj.user_organization_link_user.filter(org__org_type=org_type).first()
         return user_org_link.org.id if user_org_link else None
+
+    def org_district_id(self, obj):
+        org_type = OrganizationType.COMPANY.value if MainRoles.MENTOR.value in self.context.get(
+            "roles") else OrganizationType.COLLEGE.value
+        user_org_link = obj.user_organization_link_user.filter(org__org_type=org_type).first()
+
+        return user_org_link.org.district.id if user_org_link else None
 
     def get_college_code(self, obj):
         if user_org_link := obj.user_organization_link_user.filter(
