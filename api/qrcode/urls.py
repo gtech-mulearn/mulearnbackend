e68771f4408062757ba3401c1280d69764910e99
Mishalabdullah <github.ijvob@simplelogin.co>
2023-11-01 10:50:54 +0530

    moved qr code to profile

--- api/qrcode/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import qrcode_views
-
-urlpatterns = [
-    # Other URL patterns
-    path('generate_qr_code/', qrcode_views.QrcodeManagmentAPI.as_view(), name='generate_qr_code'),
-]
