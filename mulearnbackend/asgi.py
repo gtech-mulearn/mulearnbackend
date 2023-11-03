2d7152f27e0a8830cc12116d6f7ce9008375bdf4
jelanmathewjames <72068016+jelanmathewjames@users.noreply.github.com>
2023-11-03 22:51:02 +0530

    changing routing import after django setup

--- mulearnbackend/asgi.py
+++ mulearnbackend/asgi.py
@@ -14,10 +14,10 @@ from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from .routing import urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mulearnbackend.settings')
 django.setup()
+
+from .routing import urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
