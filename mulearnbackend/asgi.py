9f7dc34ac1ed2910fd38c2d374b1f0977586202d
Shaheen Hyder K <shaheenhyderk@gmail.com>
2023-11-03 19:16:19 +0530

    fixed asgi

--- mulearnbackend/asgi.py
+++ mulearnbackend/asgi.py
@@ -8,14 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from api.common.routers import urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mulearnbackend.settings')
+django.setup()
 
+from api.common.routers import urlpatterns
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': URLRouter(
