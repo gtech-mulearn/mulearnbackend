7c038ba61c4269a694060932585db00d4ddf8b47
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-11-24 21:50:14 +0530

    fix(lc meet api): validate data

--- api/dashboard/lc/dash_lc_serializer.py
+++ api/dashboard/lc/dash_lc_serializer.py
@@ -517,6 +517,7 @@ class MeetRecordsCreateEditDeleteSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
+        validated_data['id'] = uuid.uuid4()
         validated_data['circle_id'] = self.context.get('circle_id')
         validated_data['created_by_id'] = self.context.get('user_id')
         validated_data['updated_by_id'] = self.context.get('user_id')
@@ -539,7 +540,7 @@ class MeetRecordsCreateEditDeleteSerializer(serializers.ModelSerializer):
                 id=uuid.uuid4(),
                 user_id=user,
                 karma=Lc.KARMA.value,
-                task_id=task.id,
+                task=task,
                 updated_by_id=user_id,
                 created_by_id=user_id,
             )
