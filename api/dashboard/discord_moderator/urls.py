e58a1513d514633347dd245bb44a22ac5bca2c39
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-12-05 23:53:06 +0530

    discord moderator leaderboard

--- api/dashboard/discord_moderator/urls.py
+++ api/dashboard/discord_moderator/urls.py
@@ -5,5 +5,6 @@ from . import discord_mod_views
 urlpatterns = [
     path('tasklist/', discord_mod_views.TaskList.as_view()),
     path('pendingcounts/', discord_mod_views.PendingTasks.as_view()),
+    path('leaderboard/', discord_mod_views.PeerLeaderBoard.as_view()),
     
 ]
