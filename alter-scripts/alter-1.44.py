2e6977922dd2b889ea13c86047b6dcf5a702b529
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-24 12:07:41 +0530

    feat(alter-1.44) : create ig campus lead roles

---
+++ alter-scripts/alter-1.44.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import uuid
+from decouple import config
+import django
+
+from connection import execute
+
+os.chdir('..')
+sys.path.append(os.getcwd())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mulearnbackend.settings')
+django.setup()
+
+from utils.utils import DiscordWebhooks
+from utils.types import WebHookActions, WebHookCategory
+
+def is_role_exist(title) -> bool:
+    query = f"""SELECT id FROM role WHERE title = '{title}'"""
+    return True if execute(query) else False
+
+def get_intrest_groups():
+    query = f"""SELECT name,code, created_by FROM interest_group"""
+    return execute(query)
+
+def create_ig_lead_roles():
+    for name,code,created_by in get_intrest_groups():
+        role_name = code+' CampusLead'
+        print(role_name)
+        role_exists = is_role_exist(role_name)
+        if role_exists:
+            continue
+        query = f"""INSERT INTO role (id, title, description ,created_by, updated_by,updated_at,created_at)
+                    VALUES (
+                        '{uuid.uuid4()}',
+                        '{role_name}',
+                        '{f'Campus Lead of {name} Interest Group'}',
+                        '{created_by}',
+                        '{created_by}',
+                        UTC_TIMESTAMP,
+                        UTC_TIMESTAMP
+                    )
+            """
+        execute(query)
+        DiscordWebhooks.general_updates(
+            WebHookCategory.ROLE.value,
+            WebHookActions.CREATE.value,
+            role_name
+        )
+
+if __name__ == '__main__':
+    create_ig_lead_roles()
+    execute("UPDATE system_setting SET value = '1.44', updated_at = now() WHERE `key` = 'db.version';")
+
