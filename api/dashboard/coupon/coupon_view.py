00058c6b1a60cfc5e4e6eb5318d8dbe6335de2ee
Shaheen Hyder K <shaheenhyderk@gmail.com>
2024-02-15 04:18:47 +0530

    change coupon verify response

--- api/dashboard/coupon/coupon_view.py
+++ api/dashboard/coupon/coupon_view.py
@@ -1,12 +1,13 @@
 from rest_framework.views import APIView
 from utils.response import CustomResponse
 from db.user import UserCouponLink
+from rest_framework.response import Response
 
 
 class CouponApi(APIView):
     def post(self, request):
         if coupon_code := request.data.get('data'):
             if UserCouponLink.objects.filter(coupon=coupon_code).exists():
-                return CustomResponse(general_message="Coupon is valid").get_success_response()
-            return CustomResponse(general_message="Coupon is invalid").get_failure_response()
-        return CustomResponse(general_message="Coupon code is required").get_failure_response()
+                return Response(data={"discount_type": "Percentage", "discount_value": 100})
+            return Response(data={"discount_type": "Percentage", "discount_value": 0})
+        return Response(data={"discount_type": "Percentage", "discount_value": 0})
