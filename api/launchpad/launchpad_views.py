5b177eb0a7bca6a4551e6f1f2b8c9883d1e6971e
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-04-16 00:17:00 +0530

    modifying the launchpad leaderboard endpoint

--- api/launchpad/launchpad_views.py
+++ api/launchpad/launchpad_views.py
@@ -8,6 +8,7 @@ class Leaderboard(APIView):
     def get(self, request):
         query = """
         SELECT 
+            u.id,
             u.full_name, 
             SUM(kal.karma) AS karma,
             COALESCE(org.title, comm.title) AS org,
@@ -25,12 +26,11 @@ class Leaderboard(APIView):
                 d.name dis, 
                 s.name state
             FROM user_organization_link AS uol
-            INNER JOIN organization AS org ON org.id = uol.org_id AND org.org_type IN 
-            ("College","School","Company")
+            INNER JOIN organization AS org ON org.id = uol.org_id AND org.org_type IN ("College","School","Company")
             LEFT JOIN district AS d ON d.id = org.district_id
             LEFT JOIN zone AS z ON z.id = d.zone_id
             LEFT JOIN state AS s ON s.id = z.state_id
-            GROUP BY uol.user_id
+            GROUP BY uol.user_id, org.id, org.title, d.name, s.name
         ) AS org ON org.user_id = u.id
         LEFT JOIN (
             SELECT 
@@ -39,15 +39,15 @@ class Leaderboard(APIView):
                 org.title AS title
             FROM organization AS org
             INNER JOIN user_organization_link AS uol ON org.id = uol.org_id AND org.org_type IN ("Community")
-            GROUP BY uol.user_id
+            GROUP BY uol.user_id, org.id, org.title
         ) AS comm ON comm.user_id = u.id
         LEFT JOIN district AS d ON d.id = u.district_id
         LEFT JOIN zone AS z ON d.zone_id = z.id
         LEFT JOIN state AS s ON z.state_id = s.id
-            WHERE 
-                tl.event = "launchpad" AND
-                kal.appraiser_approved = TRUE
-        GROUP BY u.id
+        WHERE 
+            tl.event = "launchpad" AND
+            kal.appraiser_approved = TRUE
+        GROUP BY u.id, u.full_name, org.title, comm.title, COALESCE(org.dis, d.name), COALESCE(org.state, s.name)
         ORDER BY karma DESC, time_
         LIMIT 30;
         """
@@ -55,6 +55,5 @@ class Leaderboard(APIView):
             cursor.execute(query)
             results = cursor.fetchall()
             column_names = [desc[0] for desc in cursor.description]
-
             list_of_dicts = [dict(zip(column_names, row)) for row in results]
             return CustomResponse(response=list_of_dicts).get_success_response()
