6faeed83afc3cafc1065ec5b253fdedb0b5403df
Kethankrk <kethankrk@gmail.com>
2024-05-18 15:43:51 +0530

    added upvote remove view and changed url

--- api/dashboard/projects/projects_view.py
+++ api/dashboard/projects/projects_view.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from db.projects import Project
+from db.projects import Project, ProjectUpvote
 from .projects_serializer import ProjectListSerializer, ProjectSerializer
 from utils.response import CustomResponse
 from utils.permission import JWTUtils, CustomizePermission
@@ -7,7 +7,14 @@ from rest_framework.serializers import ValidationError
 
 class ProjectView(APIView):
     authentication_classes = [CustomizePermission]
-    def get(self, request):
+    def get(self, request, project_id=None):
+        if project_id:
+            try:
+                project = Project.objects.get(id=project_id)
+            except Project.DoesNotExist:
+                return CustomResponse(general_message="Project not found!").get_failure_response()
+            serializer = ProjectListSerializer(project)
+            return CustomResponse(response=serializer.data).get_success_response()
         projects = Project.objects.prefetch_related('project_contributor').all()
         serializer = ProjectListSerializer(projects, many=True)
         return CustomResponse(response=serializer.data).get_success_response()
@@ -74,3 +81,14 @@ class AddProjectUpvoteView(APIView):
             return CustomResponse(general_message="You have already upvoted this project").get_failure_response()
         project.project_upvote.create(user_id=user_id)
         return CustomResponse(general_message="Successfully upvoted").get_success_response()
+    
+    def delete(self, request, project_id=None):
+        user_id = JWTUtils.fetch_user_id(request)
+        if project_id is None:
+            return CustomResponse(general_message="Project id is required!").get_failure_response()
+        try:
+            upvote = ProjectUpvote.objects.get(project=project_id, user=user_id)
+        except ProjectUpvote.DoesNotExist:
+            return CustomResponse(general_message="You have not upvoted this project!").get_failure_response()
+        upvote.delete()
+        return CustomResponse(general_message="Successfully removed the upvote").get_success_response()
--- api/dashboard/projects/urls.py
+++ api/dashboard/projects/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("delete/<str:id>/", projects_view.ProjectView.as_view()),
     path("edit/<str:id>/", projects_view.ProjectView.as_view()),
     path("projects/", projects_view.GetUserProjectView.as_view()),
-    path("get/", projects_view.ProjectView.as_view()),
+    path("<str:project_id>/", projects_view.ProjectView.as_view()),
+    path("", projects_view.ProjectView.as_view()),
     path("upvote/<str:project_id>/", projects_view.AddProjectUpvoteView.as_view()),
 ]
