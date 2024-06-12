91f7a234f0369b2c1f2791e2c9f15f5b96eacfc5
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-06-12 13:19:06 +0530

    refactor: Update Donor model fields to allow null values

--- db/donor.py
+++ db/donor.py
@@ -11,9 +11,9 @@ class Donor(models.Model):
     currency = models.CharField(max_length=30)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
-    company = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
-    pan_number = models.CharField(max_length=10)
+    company = models.CharField(max_length=100, null=True)
+    phone_number = models.CharField(max_length=20, null=True)
+    pan_number = models.CharField(max_length=10, null=True)
     created_by = models.ForeignKey(User, on_delete=models.SET(settings.SYSTEM_ADMIN_ID), db_column='created_by', related_name='donor_created_by')
     created_at = models.DateTimeField(auto_now_add=True)
 
