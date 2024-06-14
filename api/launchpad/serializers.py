63b07a5fec49ac91bb08e9f73274277f6672cd05
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-06-14 18:54:35 +0530

    feat(launchpad details api)

--- api/launchpad/serializers.py
+++ api/launchpad/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from db.user import User
 
+
 class LaunchpadLeaderBoardSerializer(serializers.ModelSerializer):
     rank = serializers.IntegerField()
     karma = serializers.IntegerField()
@@ -12,3 +13,15 @@ class LaunchpadLeaderBoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("rank", "full_name", "karma", "org", "district_name", "state")
+
+
+class LaunchpadParticipantsSerializer(serializers.ModelSerializer):
+    org = serializers.CharField()
+    district_name = serializers.CharField()
+    state = serializers.CharField()
+    level = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ("full_name", "level", "org", "district_name", "state")
+
