b03dc7990d171df36c07f49a4d112e9d20e21119
Aswanth Vc <aswanth.abam@gmail.com>
2025-03-05 02:09:56 +0530

    fix(user): show public users only in search api

--- api/dashboard/user/dash_user_views.py
+++ api/dashboard/user/dash_user_views.py
@@ -420,11 +420,16 @@ class UserSearchAPI(APIView):
 
     def get(self, request):
         role = request.query_params.get("role")
-        queryset = User.objects.all().select_related("wallet_user")
+        queryset = (
+            User.objects.all()
+            .select_related("wallet_user")
+            .prefetch_related("user_settings_user")
+        )
         if role:
             queryset = queryset.filter(
                 user_role_link_user__role__title=role,
                 user_role_link_user__verified=True,
+                user_settings_user__is_public=True,
             )
 
         queryset = CommonUtils.get_paginated_queryset(
