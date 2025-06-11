78011eb7cc4d07b8c8bc15dbb939fa00c13a797a
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2025-06-11 12:58:41 +0530

    (chore): changed endpoint for wadhwani college and zonal leaderboard

--- api/leaderboard/serializers.py
+++ api/leaderboard/serializers.py
@@ -87,3 +87,14 @@ class StudentLeaderboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["full_name", "total_karma", "institution"]
+
+class WadhwaniCollegeLeaderboardSerializer(serializers.Serializer):
+    code = serializers.CharField()
+    title = serializers.CharField()
+    total_karma = serializers.IntegerField()
+    students = serializers.IntegerField()
+
+class WadhwaniZoneLeaderboardSerializer(serializers.Serializer):
+    zone_name = serializers.CharField()
+    total_karma = serializers.IntegerField()
+    students = serializers.IntegerField()
--- api/wadhwani/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import wadhwani_views
-
-urlpatterns = [
-    path('college/', wadhwani_views.WadhwaniCollegeLeaderboard.as_view()),
-    path('zonal/', wadhwani_views.WadhwaniZonalLeaderboard.as_view()),
-    
-]
