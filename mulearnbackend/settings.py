04f114da1b686232ff5d0d2a77d6ed214ead5ab4
Anna-Jaison <anaajai0704@gmail.com>
2025-06-26 13:18:11 +0530

    requirements added

--- mulearnbackend/settings.py
+++ mulearnbackend/settings.py
@@ -17,8 +17,6 @@ from decouple import config as decouple_config
 
 import logging
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
