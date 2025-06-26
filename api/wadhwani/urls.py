7a0fd082f196a8a6252af49a7fa33faa2fbd0e54
Anna-Jaison <anaajai0704@gmail.com>
2025-06-26 13:34:35 +0530

    deleted wadhwani

--- api/wadhwani/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import wadhwani_views
-
-urlpatterns = [
-    path('students/', wadhwani_views.WadhwaniStudentsLeaderboard.as_view()),
-    path('college/', wadhwani_views.WadhwaniCollegeLeaderboard.as_view()),
-    
-]
