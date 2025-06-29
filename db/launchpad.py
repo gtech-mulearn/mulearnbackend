a31d8063e1d6d3747612002974fba5fa3e3dc18b
Anna-Jaison <anaajai0704@gmail.com>
2025-06-29 12:21:46 +0530

    launchpad models

--- db/launchpad.py
+++ db/launchpad.py
@@ -3,6 +3,55 @@ from django.conf import settings
 from db.user import User
 from db.organization import Organization
 
+#new launchpad25 db data
+
+class LaunchpadCompanies(models.Model):
+    id= models.CharField(primary_key=True, max_length=36)
+    name = models.CharField(max_length=255, unique=True)             
+    poc_name=models.CharField(max_length=100, null=True)
+    poc_role=models.CharField(max_length=100, null=True)
+    poc_email=models.CharField(max_length=100, null=True)
+    poc_phone=models.CharField(max_length=20, null=True)
+    username= models.CharField(max_length=100, unique=True)
+    password=models.CharField(max_length=100 ,null=True)
+created_at = models.DateTimeField(auto_now_add=True)
+updated_at = models.DateTimeField(auto_now=True)
+  
+class Meta:
+    managed = False
+    db_table = 'launchpad_companies'
+
+class LaunchpadRecruters(models.Model):
+
+ id = models.CharField(primary_key=True, max_length=36)
+ company_id = models.ForeignKey(LaunchpadCompanies, on_delete=models.CASCADE, related_name="launchpad_company")
+ name = models.CharField(max_length=100)
+ email = models.CharField(max_length=100)
+ phone = models.CharField(max_length=20)
+ password = models.CharField(max_length=255)
+ role = models.CharField(max_length=50, null=True)
+ created_at = models.DateTimeField(auto_now_add=True)
+ updated_at = models.DateTimeField(auto_now=True)
+   
+class Meta:
+        managed = False
+        db_table = 'launchpad_recruiters'
+
+ class launchpad_jobs(models.Model):
+     id = models.CharField(primary_key=True, max_length=36)
+     company_id = models.ForeignKey(LaunchpadCompanies, on_delete=models.CASCADE)
+     namae = models.CharField(max_length=255)
+     skills = models.CharField(max_length=255, null=True)
+     experience = models.CharField(max_length=225, null=True)
+     domain = models.CharField(max_length=225, null=True)
+     intrest_group = models.CharField(max_length=225, null=True)
+     task_discription = models.TextField(null=True)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+class Meta:
+        managed = False
+        db_table = 'launchpad_jobs'
 
 class LaunchPadUsers(models.Model):
     id = models.CharField(primary_key=True, max_length=36)
