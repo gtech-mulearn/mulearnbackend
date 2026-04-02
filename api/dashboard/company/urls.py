9ef3258ef19e83de7fcafe289eeb835752cd30c2
Pranav P <pranavp92075@gmail.com>
2026-04-02 16:20:00 +0530

    feat: implement company onboarding and profile management modules with associated serializers and API endpoints

--- api/dashboard/company/urls.py
+++ api/dashboard/company/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 urlpatterns = [
+        path("", include("api.dashboard.company.onboarding.urls")),
+        path("profile/", include("api.dashboard.company.profile.urls")),
         path("jobs/", include("api.dashboard.company.jobs.urls")),
 ]
 
