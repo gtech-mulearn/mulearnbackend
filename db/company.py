201eb575966333b435d478eb6fc71f9dd4335e85
Pranav P <pranavp92075@gmail.com>
2026-04-02 17:49:57 +0530

    feat: add Company, CompanyJob, and CompanyJobRule models to database schema

--- db/company.py
+++ db/company.py
@@ -1,3 +1,11 @@
+import uuid
+from django.db import models
+from django.conf import settings
+from .user import User
+from .skill import Skill             # import the Skill model
+from .task import InterestGroup  # import InterestGroup model
+from .achievement import Achievement
+
 class Company(models.Model):
     STATUS_CHOICES = [
         ('pending_verification', 'Pending Verification'),
@@ -33,3 +41,69 @@ class Company(models.Model):
     deleted_at = models.DateTimeField(blank=True, null=True)
     updated_by = models.CharField(max_length=36, blank=True, null=True)
     deleted_by = models.CharField(max_length=36, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'company'
+
+
+class CompanyJob(models.Model):
+    JOB_TYPE_CHOICES = [
+        ('Hybrid', 'Hybrid'),
+        ('Full-Time', 'Full-Time'),
+        ('Remote', 'Remote'),
+        ('Part-Time', 'Part-Time'),
+        ('Internship', 'Internship'),
+        ('Gig', 'Gig'),
+    ]
+
+    STATUS_CHOICES = [
+        ('Draft', 'Draft'),
+        ('Active', 'Active'),
+        ('Closed', 'Closed'),
+        ('Expired', 'Expired'),
+    ]
+
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    company_id = models.ForeignKey(Company, on_delete=models.CASCADE, db_column='company_id', related_name='company_jobs')
+    title = models.CharField(max_length=75)
+    experience = models.CharField(max_length=20, blank=True, null=True)
+    job_description = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=75, blank=True, null=True)
+    salary_range = models.CharField(max_length=36, blank=True, null=True)
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE_CHOICES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Active')
+    is_deleted = models.BooleanField(default=False)
+    min_karma = models.IntegerField(blank=True, null=True)
+    min_level = models.SmallIntegerField(blank=True, null=True)  # TINYINT
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = False
+        db_table = 'company_jobs'
+
+class CompanyJobRule(models.Model):
+    RULE_CHOICES = [
+        ('skill', 'Skill'),
+        ('interest_group', 'Interest Group'),
+        ('achievement', 'Achievement')
+    ]
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    job = models.ForeignKey(CompanyJob,  related_name="rules",  on_delete=models.CASCADE)
+    rule_type = models.CharField(max_length=20, choices=RULE_CHOICES)
+    rule_type_id = models.CharField(max_length=36)  # generic FK
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+     
+        db_table = "company_job_rules"
+        unique_together = ('job', 'rule_type', 'rule_type_id')
+
+    @property
+    def rule_detail(self):
+        if self.rule_type == 'skill':
+            return Skill.objects.get(id=self.rule_type_id)
+        elif self.rule_type == 'interest_group':
+            return InterestGroup.objects.get(id=self.rule_type_id)
+        else:
+            return Achievement.objects.get(id=self.rule_type_id)
