cd70af1ceda779bb11693dc55f4d1a1903268bc6
Afif Showfeer <134789450+afifshowfeer@users.noreply.github.com>
2026-06-21 17:19:32 +0530

    feat: add utility to fetch formatted event types and scopes

--- api/dashboard/events/event_utils.py
+++ api/dashboard/events/event_utils.py
@@ -5,6 +5,6 @@ def get_event_types_scopes():
     Returns a combined dictionary containing formatted lists of event types and event scopes.
     """
     return {
-        "event_type": [val.replace('_', ' ').capitalize() for val in Event.EventType.values],
-        "event_scope": [val.replace('_', ' ').capitalize() for val in Event.EventScope.values],
+        "event_type": list(Event.EventType.labels),
+        "event_scope": list(Event.EventScope.labels),
     }
