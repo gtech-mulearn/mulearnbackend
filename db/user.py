43f22321e89b1066b0ea5db99b15820a5daf66de
Pranav P <pranavp92075@gmail.com>
2026-07-01 20:38:27 +0530

    feat: add Django models for user, mentor, referral, role, and social profile management

--- db/user.py
+++ db/user.py
@@ -22,7 +22,7 @@ class User(models.Model):
     email = models.EmailField(unique=True, max_length=200)
     password = models.CharField(max_length=200, blank=True, null=True)
     mobile = models.CharField(unique=True, max_length=15, blank=True, null=True)
-    gender = models.CharField(max_length=10, blank=True, null=True, choices=[("Male", "Male"), ("Female", "Female")])
+    gender = models.CharField(max_length=20, blank=True, null=True, choices=[("Male", "Male"), ("Female", "Female"), ("Other", "Other"), ("Prefer not to say", "Prefer not to say")])
     dob = models.DateField(blank=True, null=True)
     admin = models.BooleanField(default=False)
     exist_in_guild = models.BooleanField(default=False)
