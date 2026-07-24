10ac8e92ac64217670edb1aa79b37fb4b67a6a56
Pranav P <pranavp92075@gmail.com>
2026-07-25 00:30:08 +0530

    feat(interest-group): add image upload API for cover and icon images, update serializers for new fields

--- api/dashboard/ig/urls.py
+++ api/dashboard/ig/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('<str:ig_id>/impact-projects/', impact_project_view.ImpactProjectListCreateAPI.as_view()),
     path('<str:ig_id>/impact-projects/<str:project_id>/', impact_project_view.ImpactProjectDetailAPI.as_view()),
     path('<str:ig_id>/impact-projects/<str:project_id>/image/', impact_project_view.ImpactProjectImageAPI.as_view()),
+    path('<str:pk>/cover-image/', dash_ig_view.InterestGroupImageAPI.as_view(), {'image_type': 'cover'}),
+    path('<str:pk>/icon-image/', dash_ig_view.InterestGroupImageAPI.as_view(), {'image_type': 'icon'}),
     path('<str:pk>/', dash_ig_view.InterestGroupAPI.as_view()),  # for edit and delete
     path('get/<str:pk>/', dash_ig_view.InterestGroupGetAPI.as_view()),  # for edit and delete
     path('<str:pk>/join/', dash_ig_view.InterestGroupMembershipAPI.as_view()),
